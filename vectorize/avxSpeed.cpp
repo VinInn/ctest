42fda1d8981b9d3a86266d330749f883787aea7f
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-07-08 10:20:13 +0200

    now with file switch

--- vectorize/avxSpeed.cpp
+++ vectorize/avxSpeed.cpp
@@ -1,31 +1,39 @@
+// #define DIV
+using Float = float;
+// using Float = double;
+
 namespace {
 inline
-float _sum0(float const *  x, 
-           float const *  y, float const *  z, int n) {
-  float sum=0;
+Float _sum0(Float const *  x, 
+           Float const *  y, Float const *  z, int n) {
+  Float sum=0;
 #pragma GCC ivdep
   for (int i=0; i!=n; ++i)
+#ifdef DIV
+    sum += z[i]+x[i]/y[i];
+#else
     sum += z[i]+x[i]*y[i];
+#endif
   return sum;
 }
 }
 
-float __attribute__ ((target ("default")))
-sum(float const *  x,
-     float const *  y, float const *  z, int n) {
+Float __attribute__ ((target ("default")))
+sum(Float const *  x,
+     Float const *  y, Float const *  z, int n) {
   return _sum0(x,y,z,n);
 }
 
 
-float  __attribute__ ((__target__ ("arch=haswell")))
-sum(float const *  x,
-     float const *  y, float const *  z, int n) {
+Float  __attribute__ ((__target__ ("arch=haswell")))
+sum(Float const *  x,
+     Float const *  y, Float const *  z, int n) {
   return _sum0(x,y,z,n);
 }
 
-float  __attribute__ ((__target__ ("arch=nehalem")))
-sum(float const *  x,
-     float const *  y, float const *  z, int n) {
+Float  __attribute__ ((__target__ ("arch=nehalem")))
+sum(Float const *  x,
+     Float const *  y, Float const *  z, int n) {
   return _sum0(x,y,z,n);
 }
 
@@ -37,34 +45,89 @@ inline unsigned long long rdtscp() {
 }
 
 #include<cstdlib>
-#include<iostream>
+#include <fstream>
+#include <iostream>
+
 int main(int npar, char * par[]) {
 
-  float x[1024],y[1024],z[1024];
-  for (int i=0; i!=1024; ++i)
-    x[i]=y[i]=z[i]=i;
+  alignas(32) Float x[10240],y[10240],z[10240];
+  for (int i=0; i!=10240; ++i)
+    x[i]=y[i]=z[i]=(1+i)*0.1;
 
   
   int n = (npar>1) ? atoi(par[1]) : 1024;
+  int n2 = (npar>2) ? atoi(par[2]) : 1024;
+  int n3 = (npar>3) ? atoi(par[3]) : 1024;
+
 
   long long t = -rdtscp();
-  float s = _sum0(x,y,z,1024);  
+  Float s = _sum0(x,y,z,1024);  
   t += rdtscp();
-  std::cout << "generic " << t << ' ' << s << std::endl;
+  std::cout << "generic 1024 " << double(t)/1024 << ' ' << s << std::endl;
 
   t = -rdtscp();
   s = sum(x,y,z,n);
   t += rdtscp();
-  std::cout << "native 0 " << t << ' ' << s << std::endl;
+  std::cout << "native " << n << ' '<< double(t)/std::max(n,1) << ' ' << s << std::endl;
+
+  t = -rdtscp();
+  s = sum(x,y,z,n2);
+  t += rdtscp();
+  std::cout << "native "<< n2 << ' ' << double(t)/std::max(n2,1) << ' ' << s << std::endl;
+
+  t = -rdtscp();
+  s = _sum0(x,y,z,n3);
+  t += rdtscp();
+  std::cout << "generic " << n3 << ' '<< double(t)/std::max(n3,1) << ' ' << s << std::endl;
 
 
   t = -rdtscp();
   for (int i=0; i<100; ++i) 
     s += sum(x,y,z,1024);
   t += rdtscp();
-  std::cout << "native 100 " << t << ' ' << s << std::endl;
+  std::cout << "native 100*1024 " << double(t)/(100*1024) << ' ' << s << std::endl;
+
+ t = -rdtscp();
+  for (int i=0; i<1000; ++i)
+    s += sum(x,y,z,1024);
+  t += rdtscp();
+  std::cout << "native 1000*1024 " << double(t)/(1000*1024) << ' ' << s << std::endl;
+
+ t = -rdtscp();
+  for (int i=0; i<100; ++i)
+    s += sum(x,y,z,10240);
+  t += rdtscp();
+  std::cout << "native 100*10240 " << double(t)/(1000*1024) << ' ' << s << std::endl;
+
+ t = -rdtscp();
+  for (int i=0; i<100; ++i)
+    s += _sum0(x,y,z,10240);
+  t += rdtscp();
+  std::cout << "generic 100*10240 " << double(t)/(1000*1024) << ' ' << s << std::endl;
+
+  t = -rdtscp();
+  for (int i=0; i<100; ++i)
+    s += sum(x,y,z,1024);
+  t += rdtscp(); 
+  std::cout << "native 100*1024 " << double(t)/(100*1024) << ' ' << s << std::endl;
 
 
+  while (1) {
+    {
+    std::ifstream in("endAvx");
+    if (in) return 0;
+    in.close();
+    }
+    std::ifstream in("goAvx");
+    if(in) {
+     for (int i=0; i<1000; ++i)
+      s += sum(x,y,z,10240);
+    } else {
+     for (int i=0; i<1000; ++i)
+      s += _sum0(x,y,z,10240);
+    }
+    in.close();
+  }
 
   return 0;
 }
