e16ed112a04dee49fdabfbac9baa478246504f6a
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-12-10 14:44:46 +0100

    test ith real cut

--- ExprEval/test/ExprEval_t.cpp
+++ ExprEval/test/ExprEval_t.cpp
@@ -9,9 +9,10 @@ int main() {
 
 
   MyExpr::Coll c;
+  for (int i=5; i<15; ++i) { c.emplace_back(new Cand(i,1,1)); }
   MyExpr::Res r;
 
-  std::string expr = "void eval(Coll const & c, Res & r) override{ r.resize(10);}";
+  std::string expr = "void eval(Coll const & c, Res & r) override{ r.resize(c.size()); std::transform(c.begin(),c.end(),r.begin(), [](auto const & c){ return (*c).pt()>10;}); }";
 
   ExprEval parser("MyExpr",expr.c_str());
 
@@ -19,7 +20,7 @@ int main() {
 
   func->eval(c,r);
 
-  std::cout << r.size() << std::endl;
+  std::cout << r.size()  << ' '  <<  std::count(r.begin(),r.end(),true) << std::endl;
 
   return 0;
 
