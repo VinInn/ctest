37082101c0e2b19a277825b66a3650ff899df56d
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-03-29 12:22:35 +0200

    more complex example

--- atom.f90
+++ atom.f90
@@ -1,5 +1,43 @@
 program atomic
-            use iso_fortran_env
-            logical(atomic_logical_kind) :: atom[*], prev
-            call atomic_cas (atom[1], prev, .false., .true.)
+    use iso_fortran_env
+    implicit none
+    integer (atomic_logical_kind) :: atom[*],old
+    integer n, tmp, tot,i
+    integer OMP_GET_THREAD_NUM, iproc
+    real b
+    REAL, ALLOCATABLE :: a(:)
+    allocate(a(1:100))
+    a = 0.0    
+    b = 0
+    n = 50
+    atom[1]=1
+    tot = 0
+    !$OMP PARALLEL private(iproc,tmp,old) &
+    !$OMP  REDUCTION(+:B) REDUCTION(+:A)
+    iproc=0
+    IPROC=OMP_GET_THREAD_NUM()         ! thread number
+    Do
+     tmp = atom[1]
+     do while( tmp<=n)
+       call atomic_cas(atom[1],old,tmp,tmp+1)
+       if (tmp .eq. old) exit
+       tmp = atom[1]
+     end do
+     if (tmp > n) EXIT
+     b = b+1
+     a(tmp) = 1
+     a(91) = a(91)+1
+     !$OMP atomic
+     tot = tot + 1
+     write(*,*) iproc, tot, tmp, old
+    end do
+    !$OMP END PARALLEL
+
+    write (*,*) b
+    write (*,*) a
+    do i=1,2
+      write(*,*) "this is fortran " ,i
+    enddo
+
+
 end program atomic
