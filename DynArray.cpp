52bda323ad869313ba48d47d4aab25db16be3c64
Vincenzo <innocent@pb-d-128-141-131-14.cern.ch>
2015-05-28 16:07:12 +0200

    a better test

--- DynArray.cpp
+++ DynArray.cpp
@@ -3,9 +3,13 @@ class DynArray {
    T * a=nullptr;
    unsigned int s=0;
 public :
-   explicit DynArray(unsigned char * storage, unsigned int isize) : a((T*)(storage)), s(isize){
+   DynArray(unsigned char * storage, unsigned int isize) : a((T*)(storage)), s(isize){
     for (auto i=0U; i<s; ++i) new((begin()+i)) T();
    }
+   DynArray(unsigned char * storage, unsigned int isize, T const& it) : a((T*)(storage)), s(isize){
+    for (auto i=0U; i<s; ++i) new((begin()+i)) T(it);
+   }
+
    ~DynArray() { for (auto i=0U; i<s; ++i) a[i].~T(); }
 
    T & operator[](unsigned int i) { return a[i];}
@@ -18,6 +22,7 @@ public :
 };
 
 #define declareDynArray(T,n,x)  alignas(alignof(T)) unsigned char x ## _storage[sizeof(T)*n]; DynArray<T> x(x ## _storage,n)
+#define initDynArray(T,n,x,i)  alignas(alignof(T)) unsigned char x ## _storage[sizeof(T)*n]; DynArray<T> x(x ## _storage,n,i)
 
 
 struct A {
@@ -49,8 +54,13 @@ int main(int s, char **) {
   auto pa = [&](auto i) { a[1].k=0.3; return a[i].k; };
   auto pb = [&](auto i) { b[1].k=0.5; return b[i].k; };
 
-  std::cout << a[n-1].k << ' ' << pa(1) << std::endl;
+  auto loop = [&](auto k) { for(auto const & q : a) k = std::min(k,q.k); return k;};
+
+  std::cout << a[n-1].k << ' ' << pa(1) << ' ' << loop(2.3) << std::endl;
   std::cout << b[n-1].k << ' ' << pb(1) << std::endl;
+
+  initDynArray(bool,n,q,true);
+  if (q[n-1]) std::cout << "ok" << std::endl;	
 
   return 0;
 };
