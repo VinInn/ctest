03de554f410e4ab9a7c2424c975eec97c48f87f0
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-08-12 10:26:25 +0200

    precision exercise

---
+++ floatPrec/precision2015.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<cmath>
+#include<limits>
+
+template<typename T> 
+void print(T x) {
+ std::cout<< std::hexfloat << x <<' '<<  std::defaultfloat << x << std::endl;
+}
+
+
+
+
+int main() {
+
+
+  float tenth = 0.1;
+  print(tenth);
+  auto next = std::nextafter(tenth,1.f);
+  print(next);
+  auto ulp = (next-tenth);
+  print (ulp);
+  auto prec = ulp/tenth;
+  print (prec);
+
+  long long large = 1000*1000*1000;
+  std::cout << large << std::endl;
+  float flarge = large;
+  print(flarge);
+  while ( float(++large)==flarge );
+  float nlarge = large;
+  std::cout << large << std::endl;
+  print(nlarge);
+  print((nlarge-flarge)/flarge);
+
+  while ( float(++large)==nlarge );
+  nlarge = large;
+  std::cout << large << std::endl;
+  print(nlarge);
+  print((nlarge-flarge)/flarge);
+
+  auto nf = [](auto a, auto b) {
+    long long n=0;
+    while(a!=b) { a=std::nextafter(a,b); ++n;}
+    std::cout << n << std::endl;
+  };
+
+  nf(0.1f,0.2f); 
+  nf(1000.f,2000.f);
+}
+
+
+
