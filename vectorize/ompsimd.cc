6429ad2392f3fc708a9c5a4983c7434562e2960c
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-08-23 15:05:29 +0200

    add inbranch variant: do not work"
    git push

--- vectorize/ompsimd.cc
+++ vectorize/ompsimd.cc
@@ -3,6 +3,11 @@ template<int N>
 float foo(float);
 
 
+#pragma omp declare simd inbranch
+template<int N>
+float foo(float, int);
+
+
 float v0[1024];
 float v1[1024];
 float v2[1024];
@@ -15,3 +20,15 @@ void go() {
  }
 
 }
+
+
+void gone() {
+ #pragma omp simd
+ for(int i=0; i<1024; ++i) {
+   if(v2[i]>0) v0[i] = foo<2>(v2[i]);
+   else v1[i] = foo<2>(v2[i]);
+ }
+
+}
+
+
