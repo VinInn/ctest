661deb4a097f00e5e88905628dc5ed9029d467ce
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-08-24 12:38:03 +0200

    naive NN works

--- vectorize/ompsimd.cc
+++ vectorize/ompsimd.cc
@@ -5,7 +5,7 @@ float foo(float);
 
 #pragma omp declare simd inbranch
 template<int N>
-float foo(float, int);
+float foo(float,float);
 
 
 float v0[1024];
@@ -25,8 +25,10 @@ void go() {
 void gone() {
  #pragma omp simd
  for(int i=0; i<1024; ++i) {
-   if(v2[i]>0) v0[i] = foo<2>(v2[i]);
-   else v1[i] = foo<2>(v2[i]);
+   auto f = foo<2>(v2[i]);
+   if(0.f>v0[i]) 
+    v0[i] = f;
+//   else v1[i] = foo<2>(v2[i]);
  }
 
 }
