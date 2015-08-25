559ca4ef9698c65d2e1ba249db61d6a0326d7948
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-08-25 14:29:30 +0200

    and a test

--- floatPrec/vdtSIMD_t.cpp
+++ floatPrec/vdtSIMD_t.cpp
@@ -4,12 +4,17 @@
 float v0[1024];
 float v1[1024];
 float v2[1024];
+float v3[1024];
+float v4[1024];
 
 
 
 void go() {
 #pragma omp simd
- for (int i=0; i<1024; ++i) v0[i] = vdt::simd_atan2(v1[i],v2[i]);
+ for (int i=0; i<1024; ++i) { 
+     v0[i] = vdt::simd_atan2f(v1[i],v2[i]);
+     v4[i] = vdt::simd_logf(v3[i]);
+ }
 }
 
 
@@ -17,9 +22,9 @@ void go() {
 
 int main(int argc, char** ) {
 
-  v1[0] = v2[0] = .5;
+  v1[0] = v2[0] = v3[0] = 0.5;
 
   go();
 
-  std::cout << v0[0] << std::endl;
+  std::cout << v0[0] << ' ' << v4[0] << std::endl;
 }
