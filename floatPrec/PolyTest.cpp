6cecaea21073c6172b581f96603961b9b9789938
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-10-14 16:19:23 +0200

    poly

--- floatPrec/PolyTest.cpp
+++ floatPrec/PolyTest.cpp
@@ -5,16 +5,23 @@ inline float approx_expf_P(float p);
 // degree =  6   => absolute accuracy is  27 bits
 template<>
 inline float approx_expf_P<6>(float y) {
+constexpr p[] = {float(0x2.p0),float(0x2.p0),float(0x1.p0),float(0x5.55523p-4),float(0x1.5554dcp-4),float(0x4.48f41p-8),float(0xb.6ad4p-12)}
 #ifndef ESTRIN  // HORNER 
-  float p =  float(0x2.p0) + y * (float(0x2.p0) + y * (float(0x1.p0) + y * (float(0x5.55523p-4) + y * (float(0x1.5554dcp-4) + y * (float(0x4.48f41p-8) + y * float(0xb.6ad4p-12)))))) ;
+  float p =  p[0] + 
+        y * (p[1] + 
+             y * (p[2] + 
+                  y * (p[3] + 
+                       y * (p[4] + 
+                            y * (p[5] + 
+                                 y * p[6]))))) ;
 #else // ESTRIN does seem to save a cycle or two
-  float p56 = float(0x4.48f41p-8) + y * float(0xb.6ad4p-12);
-  float p34 = float(0x5.55523p-4) + y * float(0x1.5554dcp-4);
+  float p56 = p[5] + y * p[6];
+  float p34 = p[3] + y * p[4];
   float y2 = y*y;
-  float p12 = float(0x2.p0) + y; // By chance we save one operation here! Funny.
+  float p12 = p[1] + y; // By chance we save one operation here! Funny.
   float p36 = p34 + y2*p56;
   float p16 = p12 + y2*p36;
-  float p =  float(0x2.p0) + y*p16;
+  float p =  p[0] + y*p16;
 #endif
   return p;
 }
