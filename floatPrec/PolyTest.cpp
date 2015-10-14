cf64e49377d79e8c4e33eb51ab326d75aefa5d51
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-10-14 16:01:13 +0200

    poly

--- floatPrec/PolyTest.cpp
+++ floatPrec/PolyTest.cpp
@@ -5,7 +5,7 @@ inline float approx_expf_P(float p);
 // degree =  6   => absolute accuracy is  27 bits
 template<>
 inline float approx_expf_P<6>(float y) {
-#if HORNER  // HORNER 
+#ifndef ESTRIN  // HORNER 
   float p =  float(0x2.p0) + y * (float(0x2.p0) + y * (float(0x1.p0) + y * (float(0x5.55523p-4) + y * (float(0x1.5554dcp-4) + y * (float(0x4.48f41p-8) + y * float(0xb.6ad4p-12)))))) ;
 #else // ESTRIN does seem to save a cycle or two
   float p56 = float(0x4.48f41p-8) + y * float(0xb.6ad4p-12);
