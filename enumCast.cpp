161e89cacdadfa1b1b3aaf4731533c4343685321
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-02-03 16:24:46 +0100

    test clas enum

---
+++ enumCast.cpp
@@ -0,0 +1,31 @@
+#include <cstring>
+
+enum class Num { zero, one, two, tree };
+
+Num conv1(int const i) {
+  Num res; memcpy(&res,&i,sizeof(i)); return res;
+}
+
+Num conv2(int const i) {
+   return static_cast<Num>(i);
+}
+
+Num num;
+
+#include<iostream>
+int main() {
+
+  std::cout << static_cast<int>(conv1(0)) << ' ' << static_cast<int>(conv1(5)) << std::endl;
+  std::cout << static_cast<int>(conv2(0)) << ' ' << static_cast<int>(conv2(5)) << std::endl;
+
+  switch (num) {
+    case Num::zero : 
+       std::cout<< "zero" << std::endl;
+       break;
+    case Num::one : 
+        std::cout<< "one" << std::endl;
+      break;
+  }
+
+  return 0;
+}
