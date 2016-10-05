6d703b41837236703f10ccd2087bda07ace2ad04
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-10-05 12:16:38 +0200

    print raised exceptions

--- logmath.cc
+++ logmath.cc
@@ -33,14 +33,36 @@ return sum;
 // #include <features.h>
 #include <fenv.h>
 
+void testEx() {
+
+int e = std::fetestexcept(FE_ALL_EXCEPT);
+    if (e & FE_DIVBYZERO) {
+        std::cout << "division by zero\n";
+    }
+    if (e & FE_INEXACT) {
+        std::cout << "inexact\n";
+    }
+    if (e & FE_INVALID) {
+        std::cout << "invalid\n";
+    }
+    if (e & FE_UNDERFLOW) {
+        std::cout << "underflow\n";
+    }
+    if (e & FE_OVERFLOW) {
+        std::cout << "overflow\n";
+    }
+
+}
+
+
 int main(void) {
 
-  //  (void) feenableexcept( FE_DIVBYZERO );
-  //(void) feenableexcept( FE_INVALID );
-  // (void) feenableexcept( FE_OVERFLOW );
-  // (void) feenableexcept( FE_UNDERFLOW );
+//  feenableexcept( FE_DIVBYZERO );
+//  feenableexcept( FE_INVALID );
+//  feenableexcept( FE_OVERFLOW );
+//  feenableexcept( FE_UNDERFLOW );
 
-
+  testEx();
 
   sigset_t *def_set;
   def_set=&termaction.sa_mask;
@@ -52,10 +74,15 @@ int main(void) {
 
   std::cout << log(0.) << " " << exp(log(0.)) << ' ' << sqrt(-1.) << std::endl;
 
+   testEx();
+
   ::printf("test(4, 6, 0) = %f\n", test(4,6,0));
   ::printf("test(0, 2, 0) = %f\n", test(0,2,0));
   ::printf("test(-2, 3, 0) = %f\n", test(-2,3,0));
-  ::printf("nan is  = %f\n", test(0, 2, 0) + test(-2,3,0));
+  ::printf("nan is  = %f\n", test(0, 2, 0) - test(-2,3,0));
+
+  testEx();
+
       return 0;
 }
 
