89db0b8e7cee606a0ed965cd9ad372f8d4b036dd
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-02-19 11:53:51 +0100

    extern c

--- vectorize/fatlib.cc
+++ vectorize/fatlib.cc
@@ -9,6 +9,7 @@ typedef float __attribute__( ( vector_size( 64 ) ) ) float32x16_t;
 #define FATHALLO(...) void __attribute__ ((__target__ (__VA_ARGS__))) \
   fathello() { std::cout << "targer is " << __VA_ARGS__ << std::endl;}
 
+extern "C" {
 
 FATHALLO("default")
 FATHALLO("sse3")
@@ -19,7 +20,8 @@ FATHALLO("arch=core-avx2")
 FATHALLO("arch=corei7-avx")
 FATHALLO("avx512f")
 // FATHALLO()
-	 
+
+}
 
 #define FATLIB(RET,FUN) RET __attribute__ ((__target__ ("default"))) FUN \
 RET __attribute__ ((__target__ ("sse3"))) FUN \
@@ -35,8 +37,9 @@ float theFMA (float x, float y, float z) { return x+y*z;}
 #define FATFMA myfma(float x, float y, float z) { return theFMA(x,y,z);} 
 #define FATFMARETURN float
 
-
+extern "C" {
 FATLIB(FATFMARETURN,FATFMA)
+}
 
 float __attribute__ ((__target__ ("default")))
 mySum(float vx, float vy) {
