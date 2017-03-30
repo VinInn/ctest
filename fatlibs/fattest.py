293f537674d92214e1d16cf0daabf91cedb6e8d6
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-03-30 11:15:32 +0200

    and its test

---
+++ fatlibs/fattest.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy
+import ctypes
+import subprocess
+print "bha"
+print subprocess.check_output("echo here am",stderr=subprocess.STDOUT,shell=True)
+print subprocess.check_output("c++ -v; exit 0",stderr=subprocess.STDOUT,shell=True)
+print subprocess.check_output("ls -l;c++ fatlib.cc -fPIC -shared -o fatlibHere.dylib; exit 0",stderr=subprocess.STDOUT,shell=True)
+print "bho"
+lib = numpy.ctypeslib.load_library('fatlibHere','.')
+hello = lib.fathello
+hello.restype = ctypes.c_char_p
+print hello()
+print "bhe"
+fma = lib.myfma
+fma.restype = ctypes.c_float
+fma.argtypes = [ctypes.c_float,ctypes.c_float,ctypes.c_float]
+fma(1,2,3)
+
+
+# In[ ]:
+
+
+
