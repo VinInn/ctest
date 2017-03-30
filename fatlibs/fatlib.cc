70f4b17d746463c86d8a89bd3daf882622325f53
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-03-30 15:30:20 +0200

    hide private functions

--- fatlibs/fatlib.cc
+++ fatlibs/fatlib.cc
@@ -5,12 +5,13 @@ typedef float __attribute__( ( vector_size( 16 ) ) ) float32x4_t;
 typedef float __attribute__( ( vector_size( 32 ) ) ) float32x8_t;
 typedef float __attribute__( ( vector_size( 64 ) ) ) float32x16_t;
 
+namespace {
 static std::string fathi;
-
+}
 #define FATHALLO(...) char const * __attribute__ ((__target__ (__VA_ARGS__))) \
   fathelloCPP() { fathi = std::string("targer is ")+__VA_ARGS__; return fathi.c_str();}
 
-
+namespace {
 FATHALLO("default")
 FATHALLO("sse3")
 FATHALLO("arch=corei7")
@@ -20,6 +21,7 @@ FATHALLO("arch=core-avx2")
 FATHALLO("arch=corei7-avx")
 FATHALLO("avx512f")
 // FATHALLO()
+}
 
 extern "C" {
   char const * fathello() { return fathelloCPP();}
@@ -37,10 +39,13 @@ RET __attribute__ ((__target__ ("avx512f"))) FUN
 inline
 float theFMA (float x, float y, float z) { return x+y*z;}
 
+
 #define FATFMA myfmaCPP(float x, float y, float z) { return theFMA(x,y,z);} 
 #define FATFMARETURN float
 
+namespace {
 FATLIB(FATFMARETURN,FATFMA)
+}
 
 extern "C" {
   float myfma(float x, float y, float z) { return myfmaCPP(x,y,z);} 
