f1037ae12d564084180aa394b4cdd5f6f17ca136
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-04-10 09:25:00 +0200

    make it template

--- vectorize/minloc.cc
+++ vectorize/minloc.cc
@@ -1,28 +1,47 @@
 #include <x86intrin.h>
+
+
+namespace minlocDetails {
+
 #ifdef __AVX512F__
-#define NATIVE_VECT_LENGH 64
+  constexpr unsigned int NATIVE_VECT_LENGH=64;
 #elif __AVX__
-#define NATIVE_VECT_LENGH 32
+  constexpr unsigned int NATIVE_VECT_LENGH=32;
 #else
-#define NATIVE_VECT_LENGH 16
+  constexpr unsigned int NATIVE_VECT_LENGH=16;
 #endif
 
-typedef float __attribute__( ( vector_size( NATIVE_VECT_LENGH ) ) ) vfloat32_t;
-typedef float __attribute__( ( vector_size( NATIVE_VECT_LENGH ) , aligned(4) ) ) vfloat32a4_t;
-typedef int __attribute__( ( vector_size( NATIVE_VECT_LENGH ) ) ) vint32_t;
+  template<typename T> struct Traits{};
 
+  template<> struct Traits<float>{
+    typedef float __attribute__( ( vector_size( NATIVE_VECT_LENGH ) ) ) vfloat_t;
+    typedef float __attribute__( ( vector_size( NATIVE_VECT_LENGH ) , aligned(4) ) ) vfloatAN_t;
+    typedef int __attribute__( ( vector_size( NATIVE_VECT_LENGH ) ) ) vint_t;
+  };
 
-inline
-vfloat32_t load(float const * x) {
-   return *(vfloat32a4_t const *)(x);
+  template<> struct Traits<double>{
+    typedef double __attribute__( ( vector_size( NATIVE_VECT_LENGH ) ) ) vfloat_t;
+    typedef double __attribute__( ( vector_size( NATIVE_VECT_LENGH ) , aligned(8) ) ) vfloatAN_t;
+    typedef long long __attribute__( ( vector_size( NATIVE_VECT_LENGH ) ) ) vint_t;
+  };
+  
+  template<typename T>
+  inline
+  typename Traits<T>::vfloat_t load(T const * x) {
+    return *(typename Traits<T>::vfloatAN_t const *)(x);
+  }
 }
 
+template<typename T>
+int minloc(T const * x, int N) {
+  using namespace minlocDetails;
+  using vfloat_t = typename Traits<T>::vfloat_t;
+  using vint_t = typename Traits<T>::vint_t;
+  constexpr int TSIZE = sizeof(T);
+  vfloat_t v0;
+  vint_t index;
 
-int minloc(float const * x, int N) {
-  vfloat32_t v0;
-  vint32_t index;
-
-  constexpr int WIDTH = NATIVE_VECT_LENGH/4; 
+  constexpr int WIDTH = NATIVE_VECT_LENGH/TSIZE; 
 
   auto M = WIDTH*(N/WIDTH);
   for (int i=M; i<N; ++i) {
@@ -33,7 +52,7 @@ int minloc(float const * x, int N) {
     v0[i-M] = x[0];
     index[i]=0;
   }
-  vint32_t j;  for (int i=0;i<WIDTH; ++i) j[i]=i;  // can be done better
+  vint_t j;  for (int i=0;i<WIDTH; ++i) j[i]=i;  // can be done better
   for (int i=0; i<M; i+=WIDTH) {
     decltype(auto) v = load(x+i);
     index =  (v<v0) ? j : index;
@@ -46,7 +65,8 @@ int minloc(float const * x, int N) {
 }
 
 
-int lmin(float const *  x, int N) {
+template<typename T>
+int lmin(T const *  x, int N) {
   int k=0;
   for (int i=1; i<N; ++i) {
     k =  (x[i] < x[k]) ? i : k;
@@ -63,11 +83,13 @@ inline unsigned long long rdtscp() {
 }
 
 
-int main() {
+template<typename T>
+int go() {
+  using namespace minlocDetails;
 
-   std::cout << "NATIVE_VECT_LENGH " << NATIVE_VECT_LENGH << std::endl;
+  std::cout << "NATIVE_VECT_LENGH " << NATIVE_VECT_LENGH << std::endl;
   
-  int N = 1024*4;
+  int N = 1024*4+3;
   float x[N];
   for (int i=0; i<N; ++i) x[i]= i%2 ? i : -i;
   for (int i = 0; i<10; ++i) {
@@ -82,6 +104,11 @@ int main() {
     std::cout << "min is at " << l1 << ' ' << ts << std::endl;
     std::cout << "minloc " << l2 << ' ' << tv << std::endl;
   }
-  return 0;
+  return N;
 
+}
+
+int main() {
+  return go<float>() + go<double>();
+   
 }
