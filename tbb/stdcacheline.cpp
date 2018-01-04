772ae256f075e27de5ab9e14bb628c2968185a9a
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-01-04 15:40:42 +0100

    not implemented yet

---
+++ tbb/stdcacheline.cpp
@@ -0,0 +1,9 @@
+#include<new>
+#include<iostream>
+
+int main() {
+
+   std::cout << std::hardware_destructive_interference_size << ' ' << std::hardware_constructive_interference_size << std::endl;
+
+   return 0;
+}
