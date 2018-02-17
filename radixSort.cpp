c02d991e0f202888b1edbf9be59772d3ab7fd851
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-02-17 15:54:51 +0100

    50 times random

--- radixSort.cpp
+++ radixSort.cpp
@@ -7,6 +7,9 @@ void radixSort(int16_t const * a, uint32_t * ind, uint32_t size) {
   auto j = ind;
   uint32_t ind2[size];
   auto k = ind2;
+
+  for (uint32_t  i = 0; i < size; i++) j[i]=i;
+  
   for (int p = 0; p < w/d; p++) {
     int c[sb];
     for (int i = 0; i < sb; i++) c[i]=0;
@@ -53,19 +56,22 @@ int main() {
   constexpr int N=12000;
   int16_t v[N];
   uint32_t ind[N];
-
+  
   for (int i = 0; i < N; i++) {
-    ind[i]=i; v[i]=i%32768; if(i%2) v[i]=-v[i];
+    v[i]=i%32768; if(i%2) v[i]=-v[i];
   }
-
-  radixSort(v,ind,N);
-
-  std::cout << v[ind[10]] << ' ' << v[ind[11000]] << std::endl;
-  std::cout << v[ind[5999]] << ' ' << v[ind[6000]] << ' ' << v[ind[6001]] << std::endl;
-  for (int i = 1; i < N; i++) {
-    if (v[ind[i]]<v[ind[i-1]])
-      std::cout << "not ordered at " << ind[i] << " : "
-		<< v[ind[i]] <<' '<< v[ind[i-1]] << std::endl;
+  
+  for (int i=0; i<50; ++i) {
+    std::random_shuffle(v,v+N);
+    radixSort(v,ind,N);
+    
+    std::cout << v[ind[10]] << ' ' << v[ind[11000]] << std::endl;
+    std::cout << v[ind[5999]] << ' ' << v[ind[6000]] << ' ' << v[ind[6001]] << std::endl;
+    for (int i = 1; i < N; i++) {
+      if (v[ind[i]]<v[ind[i-1]])
+	std::cout << "not ordered at " << ind[i] << " : "
+		  << v[ind[i]] <<' '<< v[ind[i-1]] << std::endl;
+    }
   }
   return 0;
 }
