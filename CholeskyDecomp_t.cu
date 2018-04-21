f8516d6df31510fd039e872a4fd3d377ebb3f572
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-04-21 10:46:46 +0200

    timing

--- CholeskyDecomp_t.cu
+++ CholeskyDecomp_t.cu
@@ -75,6 +75,12 @@ void genMatrix(M  & m ) {
 
 int main() {
 
+  auto start = std::chrono::high_resolution_clock::now();
+  auto delta = start - start;
+  auto delta1 = delta;
+  auto delta2 = delta;
+
+  
   if (cuda::device::count() == 0) {
 	std::cerr << "No CUDA devices on this system" << "\n";
 	exit(EXIT_FAILURE);
@@ -92,45 +98,61 @@ int main() {
 
   std::cout << mm[SIZE/2](1,1) << std::endl;
 
+  for (int kk=0; kk<100; ++kk) {
   
-  auto m_d = cuda::memory::device::make_unique<MX[]>(current_device, SIZE);
+    auto m_d = cuda::memory::device::make_unique<MX[]>(current_device, SIZE);
+    
+    cuda::memory::copy(m_d.get(), &mm, SIZE*sizeof(MX));
+    
+    int threadsPerBlock =128;
+    int blocksPerGrid = SIZE/threadsPerBlock;
+    
+    delta -= (std::chrono::high_resolution_clock::now()-start);
+    
+    cuda::launch(
+		 invert<4>,
+		 { blocksPerGrid, threadsPerBlock },
+		 m_d.get(),SIZE
+		 );
+    
+    cuda::memory::copy(&mm, m_d.get(),SIZE*sizeof(MX));
+    delta += (std::chrono::high_resolution_clock::now()-start);
+    
+    if (0==kk) std::cout << mm[SIZE/2](1,1) << std::endl;
+    
+    
+    delta1 -= (std::chrono::high_resolution_clock::now()-start);
+    
+    cuda::launch(
+		 invertSeq<4>,
+		 { blocksPerGrid, threadsPerBlock },
+		 m_d.get(),SIZE
+		 );
+    
+    cuda::memory::copy(&mm, m_d.get(),SIZE*sizeof(MX));
+    delta2 += (std::chrono::high_resolution_clock::now()-start);
+    
+    if (0==kk) std::cout << mm[SIZE/2](1,1) << std::endl;
+    
   
-  cuda::memory::copy(m_d.get(), &mm, SIZE*sizeof(MX));
+    delta2 -= (std::chrono::high_resolution_clock::now()-start);
+    for ( auto & m : mm) {
+      ROOT::Math::CholeskyDecomp<double,4> decomp(m);
+      assert(decomp);
+      assert(decomp.Invert(m));
+    }
+    delta2 += (std::chrono::high_resolution_clock::now()-start);
 
-  int threadsPerBlock =128;
-  int blocksPerGrid = SIZE/threadsPerBlock;
-
-  cuda::launch(
-	       invert<4>,
-	       { blocksPerGrid, threadsPerBlock },
-	       m_d.get(),SIZE
-	       );
-
-  cuda::memory::copy(&mm, m_d.get(),SIZE*sizeof(MX));
-  
-  std::cout << mm[SIZE/2](1,1) << std::endl;
-  
-  cuda::launch(
-	       invertSeq<4>,
-	       { blocksPerGrid, threadsPerBlock },
-	       m_d.get(),SIZE
-	       );
-
-  cuda::memory::copy(&mm, m_d.get(),SIZE*sizeof(MX));
-  
-  std::cout << mm[SIZE/2](1,1) << std::endl;
-  
-  
-  
-  for ( auto & m : mm) {
-    ROOT::Math::CholeskyDecomp<double,4> decomp(m);
-    assert(decomp);
-    assert(decomp.Invert(m));
   }
   
   std::cout << mm[SIZE/2](1,1) << std::endl; 
   
- 
+  std::cout <<"cuda/cudaSeq/x86 computation took "
+	    << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()/100. << ' '
+	    << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()/100.  << ' '
+	    << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()/100.  << ' '
+	    << " ms" << std::endl;
+}
   
    return 0;
 }
