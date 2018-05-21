3c8a7743792371073a2c8072a3492321e3128666
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-05-21 17:38:18 +0200

    radixsort fixed

--- radixSort.cu
+++ radixSort.cu
@@ -1,134 +1,4 @@
-#include<cstdint>
-#include<cassert>
-#include<cstdio>
-
-template<typename T>
-__device__  
-void radixSort(T * a, uint16_t * ind, uint32_t size) {
-    
-  constexpr int d = 8, w = 8*sizeof(T);
-  constexpr int sb = 1<<d;
-
-  constexpr int MaxSize = 256*32;
-  __shared__ uint16_t ind2[MaxSize];
-  __shared__ int32_t c[sb], ct[sb], cu[sb];
-  __shared__ uint32_t firstNeg;    
-
-  assert(size<=MaxSize); 
-  assert(blockDim.x==sb);  
-
-  // bool debug = false; // threadIdx.x==0 && blockIdx.x==5;
-
-  firstNeg=0;
-
-  auto j = ind;
-  auto k = ind2;
-
-  int32_t first = threadIdx.x;
-  for (auto i=first; i<size; i+=blockDim.x)  j[i]=i;
-  __syncthreads();
-
-
-  for (int p = 0; p < w/d; ++p) {
-    c[threadIdx.x]=0;
-    __syncthreads();
-
-    // fill bins
-    for (auto i=first; i<size; i+=blockDim.x) {
-      auto bin = (a[j[i]] >> d*p)&(sb-1);
-      atomicAdd(&c[bin],1);
-    }
-    __syncthreads();
-
-    // prefix scan "optimized"???...
-    auto x = c[threadIdx.x];
-    auto laneId = threadIdx.x & 0x1f;
-    #pragma unroll
-    for( int offset = 1 ; offset < 32 ; offset <<= 1 ) {
-      auto y = __shfl_up_sync(0xffffffff,x, offset);
-      if(laneId >= offset) x += y;
-    }
-    ct[threadIdx.x] = x;
-    __syncthreads();
-    auto ss = (threadIdx.x/32)*32 -1;
-    c[threadIdx.x] = ct[threadIdx.x];
-    for(int i=ss; i>0; i-=32) c[threadIdx.x] +=ct[i]; 
-
-    /* prefix scan for the nulls
-    if (threadIdx.x==0)
-      for (int i = 1; i < sb; ++i) c[i] += c[i-1];
-    */
-    __syncthreads();
-
-
-    for (int i=size-first-1; i>=0; i-=blockDim.x) {
-       cu[threadIdx.x]=-1;
-       auto bin = (a[j[i]] >> d*p)&(sb-1);
-       ct[threadIdx.x]=bin;
-       atomicMax(&cu[bin],int(i));
-       __syncthreads();
-       if (i==cu[bin])  // ensure to keep them in order
-         for (int ii=threadIdx.x; ii<blockDim.x; ++ii) if (ct[ii]==bin) {auto oi = ii-threadIdx.x; k[--c[bin]] = j[i-oi]; }
-       __syncthreads();
-     }    
- 
-    /*  
-    // broadcast for the nulls
-    if (threadIdx.x==0)
-    for (int i=size-first-1; i>=0; i--) { // =blockDim.x) {
-      auto ik = atomicSub(&c[(a[j[i]] >> d*p)&(sb-1)],1);
-      k[ik-1] = j[i];
-    }
-    */
-
-    __syncthreads();
-    assert(c[0]==0);
-
-
-    // swap (local, ok)
-    auto t=j;j=k;k=t;
-  }
-
-  // w/d is even so ind is correct
-  assert(j==ind);
-  __syncthreads();
-
-  
-
-  // now move negative first...
-  // find first negative  (for float ^ will not work...)
-  for (auto i=first; i<size-1; i+=blockDim.x) {
-    // if ( (int(a[ind[i]])*int(a[ind[i+1]])) <0 ) firstNeg=i+1;
-   if ( (a[ind[i]]^a[ind[i+1]]) < 0 ) firstNeg=i+1; 
-  }
-  
-  __syncthreads();
-  // assert(firstNeg>0); not necessary true if all positive !
-
-  auto ii=first;
-  for (auto i=firstNeg+threadIdx.x; i<size; i+=blockDim.x)  { ind2[ii] = ind[i]; ii+=blockDim.x; }
-  __syncthreads();
-  ii= size-firstNeg +threadIdx.x;
-  assert(ii>=0);
-  for (auto i=first;i<firstNeg;i+=blockDim.x)  { ind2[ii] = ind[i]; ii+=blockDim.x; }
-  __syncthreads();
-  for (auto i=first; i<size; i+=blockDim.x) ind[i]=ind2[i];
-
-  
-}
-
-
-template<typename T>
-__global__
-void radixSortMulti(T * v, uint16_t * index, uint32_t * offsets) {
-
-  auto a = v+offsets[blockIdx.x];
-  auto ind = index+offsets[blockIdx.x];;
-  auto size = offsets[blockIdx.x+1]-offsets[blockIdx.x];
-
-  radixSort(a,ind,size);
-
-}
+#include "radixSort.h"
 
 #include "cuda/api_wrappers.h"
 
@@ -168,8 +38,10 @@ std::uniform_int_distribution<T> rgen(std::numeric_limits<T>::min(),std::numeric
   T v[N];
   uint16_t ind[N];
 
-  std::cout << "Will sort " << N << " 'ints' of size " << sizeof(T) << std::endl;
 
+
+
+  std::cout << "Will sort " << N << " 'ints' of size " << sizeof(T) << std::endl;
 
   for (int i=0; i<50; ++i) {
 
@@ -186,8 +58,14 @@ std::uniform_int_distribution<T> rgen(std::numeric_limits<T>::min(),std::numeric
 
   uint32_t offsets[blocks+1];
   offsets[0]=0;
-  for (int j=1; j<blocks+1; ++j) offsets[j] = offsets[j-1]+blockSize;
+  for (int j=1; j<blocks+1; ++j) { offsets[j] = offsets[j-1]+blockSize-3*j; assert(offsets[j]<=N);}
 
+  if (i==1) { // special cases...
+    offsets[0]=0; offsets[1]=0;  offsets[2]=19;offsets[3]=32+offsets[2];
+    offsets[4]=123+offsets[3];offsets[5]=256+offsets[4];offsets[6]=311+offsets[5]; 
+    offsets[7]=2111+offsets[6];offsets[8]=256*11+offsets[7]; offsets[9]=44+offsets[8];
+    offsets[10]=3297+offsets[9];
+  }
 
   std::random_shuffle(v,v+N);
   auto v_d = cuda::memory::device::make_unique<T[]>(current_device, N);
@@ -197,21 +75,23 @@ std::uniform_int_distribution<T> rgen(std::numeric_limits<T>::min(),std::numeric
   cuda::memory::copy(v_d.get(), v, N*sizeof(T));
   cuda::memory::copy(off_d.get(), offsets, 4*(blocks+1));
 
+  if (i<2) std::cout << "lauch for " << offsets[blocks] << std::endl;
 
-   int threadsPerBlock =256;
-   int blocksPerGrid = blocks;
    delta -= (std::chrono::high_resolution_clock::now()-start);
    cuda::launch(
-                radixSortMulti<T>,
-                { blocksPerGrid, threadsPerBlock },
+                radixSortMultiWrapper<T>,
+                { blocks, 256 },
                 v_d.get(),ind_d.get(),off_d.get()
         );
 
+ if (i==0) std::cout << "done for " << offsets[blocks] << std::endl;
 
 //  cuda::memory::copy(v, v_d.get(), 2*N);
    cuda::memory::copy(ind, ind_d.get(), 2*N);
 
    delta += (std::chrono::high_resolution_clock::now()-start);
+
+  if (i==0) std::cout << "done for " << offsets[blocks] << std::endl;
 
   if (32==i) {
     std::cout << v[ind[0]] << ' ' << v[ind[1]] << ' ' << v[ind[2]] << std::endl;
@@ -219,7 +99,7 @@ std::uniform_int_distribution<T> rgen(std::numeric_limits<T>::min(),std::numeric
     std::cout << v[ind[blockSize/2-1]] << ' ' << v[ind[blockSize/2]] << ' ' << v[ind[blockSize/2+1]] << std::endl;
   }
   for (int ib=0; ib<blocks; ++ib)
-  for (int i = offsets[ib]+1; i < offsets[ib+1]; i++) {
+  for (auto i = offsets[ib]+1; i < offsets[ib+1]; i++) {
       auto a = v+offsets[ib];
    // assert(!(a[ind[i]]<a[ind[i-1]]));
      if (a[ind[i]]<a[ind[i-1]])
