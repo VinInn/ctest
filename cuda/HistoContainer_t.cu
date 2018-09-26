b31deec3ccd1cbb3f7497dbd9779c13985e8cd16
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-09-26 19:18:22 +0200

    add new versions

--- cuda/HistoContainer_t.cu
+++ cuda/HistoContainer_t.cu
@@ -1,129 +1,112 @@
 #include "HistoContainer.h"
 
-#include <algorithm>
-#include <cassert>
-#include <iostream>
-#include <limits>
-#include <random>
+#include<algorithm>
+#include<cassert>
+#include<iostream>
+#include<random>
+#include<limits>
 
-#include <cuda/api_wrappers.h>
-
-template<typename T>
+template<typename T, int NBINS=128, int S=8*sizeof(T), int DELTA=1000>
 void go() {
+  std::mt19937 eng;
 
-  if (cuda::device::count() == 0) {
-	std::cerr << "No CUDA devices on this system" << "\n";
-	exit(EXIT_FAILURE);
+  int rmin=std::numeric_limits<T>::min();
+  int rmax=std::numeric_limits<T>::max();
+  if (NBINS!=128) {
+    rmin=0;
+    rmax=NBINS*2-1;
   }
 
-  auto current_device = cuda::device::current::get(); 
- 
 
 
-  std::mt19937 eng;
-  std::uniform_int_distribution<T> rgen(std::numeric_limits<T>::min(),std::numeric_limits<T>::max());
+  std::uniform_int_distribution<T> rgen(rmin,rmax);
 
   
   constexpr int N=12000;
   T v[N];
-  auto v_d = cuda::memory::device::make_unique<T[]>(current_device, N);
 
-  cuda::memory::copy(v_d.get(), v, N*sizeof(T));
-
-  constexpr uint32_t nParts = 10;
-  constexpr uint32_t partSize = N/nParts;
-  uint32_t offsets[nParts+1];
-
-  using Hist = HistoContainer<T,7,8>;
-  std::cout << "HistoContainer " << Hist::nbins() << ' ' << Hist::binSize() << ' ' << (std::numeric_limits<T>::max()-std::numeric_limits<T>::min())/Hist::nbins() << std::endl;
-  
-  Hist h[nParts];
-
-  auto h_d = cuda::memory::device::make_unique<Hist[]>(current_device, nParts);
-  auto off_d = cuda::memory::device::make_unique<uint32_t[]>(current_device, nParts+1);
+  using Hist = HistoContainer<T,NBINS,N,S>;
+  using Hist4 = HistoContainer<T,NBINS,N,S,uint16_t,4>;
+  std::cout << "HistoContainer " << Hist::nbits() << ' ' << Hist::nbins() << ' ' << Hist::totbins() << ' ' << Hist::capacity() << ' ' << (rmax-rmin)/Hist::nbins() << std::endl;
+  std::cout << "bins " << int(Hist::bin(0)) << ' ' <<  int(Hist::bin(rmin)) << ' ' << int(Hist::bin(rmax)) << std::endl;  
+  std::cout << "HistoContainer4 " << Hist4::nbits() << ' ' << Hist4::nbins() << ' ' << Hist4::totbins() << ' ' << Hist4::capacity() << ' ' << (rmax-rmin)/Hist::nbins() << std::endl;
+  for (auto nh=0; nh<4; ++nh) std::cout << "bins " << int(Hist4::bin(0))+Hist4::histOff(nh) << ' ' <<  int(Hist::bin(rmin))+Hist4::histOff(nh) << ' ' << int(Hist::bin(rmax))+Hist4::histOff(nh) << std::endl;
 
 
+  Hist h;
+  Hist4 h4;
+  typename Hist::Counter ws[Hist::totbins()];
+  typename Hist4::Counter ws4[Hist4::totbins()];
   for (int it=0; it<5; ++it) {
-
-    offsets[0]=0;
-    for (uint32_t j=1; j<nParts+1; ++j) { offsets[j] = offsets[j-1]+partSize-3*j; assert(offsets[j]<=N);}
-
-    if (it==1) { // special cases...
-      offsets[0]=0; offsets[1]=0;  offsets[2]=19;offsets[3]=32+offsets[2];
-      offsets[4]=123+offsets[3];offsets[5]=256+offsets[4];offsets[6]=311+offsets[5];
-      offsets[7]=2111+offsets[6];offsets[8]=256*11+offsets[7]; offsets[9]=44+offsets[8];
-      offsets[10]=3297+offsets[9];
-    }
-
-
-
-    cuda::memory::copy(off_d.get(), offsets, 4*(nParts+1));
-
-
     for (long long j = 0; j < N; j++) v[j]=rgen(eng);
+    if (it==2) for (long long j = N/2; j < N/2+N/4; j++) v[j]=4;
+    h.zero();h4.zero();
+    assert(h.size()==0);assert(h4.size()==0);
+    for (auto & i: ws) i=0; 
+    for (auto & i: ws4) i=0;
+    for (long long j = 0; j < N; j++) { h.count(v[j]); if(j<2000) h4.count(v[j],2); else h4.count(v[j],j%4); }
+    h.finalize(); h4.finalize();
+    assert(h.off[0]==0);
+    assert(h.size()==N);
+    assert(h4.off[0]==0);
+    assert(h4.size()==N);
+    for (long long j = 0; j < N; j++) { h.fill(v[j],j,ws);  if(j<2000) h4.fill(v[j],j,ws4,2); else h4.fill(v[j],j,ws4,j%4); }
+    assert(h.size()==N);
+    assert(h4.size()==N);
 
-    if (it==2) { // spill 
-       for (long long j = 1000; j < 2000; j++) v[j]= sizeof(T)==1 ? 22 : 3456;
-    }
-
-
-    cuda::memory::copy(v_d.get(), v, N*sizeof(T));
-
-    cudautils::fillManyFromVector(h_d.get(),nParts,v_d.get(),off_d.get(),offsets[10],256,0);
-
-    cuda::memory::copy(&h, h_d.get(), nParts*sizeof(Hist));                                
-
-        
-    auto verify = [&](uint32_t i, uint32_t k, uint32_t t1, uint32_t t2) {
+    auto verify = [&](uint32_t i, uint32_t j, uint32_t k, uint32_t t1, uint32_t t2) {
       assert(t1<N); assert(t2<N);
-      if ( T(v[t1]-v[t2])<=0) std::cout << "for " << i <<':'<< v[k] <<" failed " << v[t1] << ' ' << v[t2] << std::endl;
+      if (i!=j && T(v[t1]-v[t2])<=0) std::cout << "for " << i <<':'<< v[k] <<" failed " << v[t1] << ' ' << v[t2] << std::endl;
     };
 
-    auto incr = [](auto & k) { return k = (k+1)%Hist::nbins();};
-
-    // make sure it spans 3 bins... 
-    auto window = T(1300);
-    
-    for (uint32_t j=0; j<nParts; ++j) {
-      std::cout << j << ": nspills " << h[j].nspills << std::endl;
-      for (uint32_t i=0; i<Hist::nbins(); ++i) {
-        if (0==h[j].size(i)) continue;
-        auto k= *h[j].begin(i);
-        if (j%2) k = *(h[j].begin(i)+(h[j].end(i)-h[j].begin(i))/2);
-        auto bk = h[j].bin(v[k]);
-        assert(bk==i);
-        assert(k<offsets[j+1]);
-        auto kl = h[j].bin(v[k]-window);
-        auto kh = h[j].bin(v[k]+window);
-        assert(kl!=i);  assert(kh!=i);
-        // std::cout << kl << ' ' << kh << std::endl;
-
-        auto me = v[k];
-        auto tot = 0;
-        auto nm = 0;
-        bool l = true; auto khh = kh; incr(khh);
-        for (auto kk=kl; kk!=khh; incr(kk)) {
-          if (kk!=kl && kk!=kh) nm+=h[j].size(kk);
-          for(auto p=h[j].begin(kk); p<h[j].end(kk); ++p) {
-           if ( std::min(std::abs(T(v[*p]-me)), std::abs(T(me-v[*p]))) > window ) {} else {++tot;}
-          }
-          if (kk==i) { l=false; continue; }
-          if (l) for (auto p=h[j].begin(kk); p<h[j].end(kk); ++p) verify(i,k,k,(*p));
-          else for (auto p=h[j].begin(kk); p<h[j].end(kk); ++p) verify(i,k,(*p),k);
-        }
-        if (h[j].nspills==0 && !(tot>=nm)) {
-           std::cout << "too bad " << j << ' ' << i <<' ' << me << '/'<< T(me-window)<< '/'<< T(me+window) << ": " << kl << '/' << kh << ' '<< khh << ' '<< tot<<'/'<<nm << std::endl;
-        }
-        if (l) std::cout << "what? " << j << ' ' << i <<' ' << me << '/'<< T(me-window)<< '/'<< T(me+window) << ": " << kl << '/' << kh << ' '<< khh << ' '<< tot<<'/'<<nm << std::endl;
-        assert(!l);
-      }
+    for (uint32_t i=0; i<Hist::nbins(); ++i) {
+      if (0==h.size(i)) continue;
+      auto k= *h.begin(i);
+      assert(k<N);
+      auto kl = NBINS!=128 ? h.bin(std::max(rmin,v[k]-DELTA)) : h.bin(v[k]-T(DELTA));
+      auto kh =	NBINS!=128 ? h.bin(std::min(rmax,v[k]+DELTA)) : h.bin(v[k]+T(DELTA));
+      if(NBINS==128) { assert(kl!=i);  assert(kh!=i); }
+      if(NBINS!=128) { assert(kl<=i);  assert(kh>=i); }
+      // std::cout << kl << ' ' << kh << std::endl;
+      for (auto j=h.begin(kl); j<h.end(kl); ++j) verify(i,kl, k,k,(*j));
+      for (auto	j=h.begin(kh); j<h.end(kh); ++j) verify(i,kh, k,(*j),k);
     }
   }
+
+  for (long long j = 0; j < N; j++) {
+    auto b0 = h.bin(v[j]);
+    int w=0;
+    int tot=0;
+    auto ftest = [&](int k) {
+       assert(k>=0 && k<N);
+       tot++;
+    };
+    forEachInBins(h,v[j],w,ftest);
+    int rtot = h.end(b0)-h.begin(b0);
+    assert(tot==rtot);
+    w=1; tot=0;
+    forEachInBins(h,v[j],w,ftest);
+    int bp = b0+1;
+    int bm = b0-1;
+    if (bp<int(h.nbins())) rtot += h.end(bp)-h.begin(bp);
+    if (bm>=0) rtot += h.end(bm)-h.begin(bm);
+    assert(tot==rtot);
+    w=2; tot=0;
+    forEachInBins(h,v[j],w,ftest);
+    bp++;
+    bm--;
+    if (bp<int(h.nbins())) rtot += h.end(bp)-h.begin(bp);
+    if (bm>=0) rtot += h.end(bm)-h.begin(bm);
+    assert(tot==rtot);
+  }
+
 }
 
 int main() {
   go<int16_t>();
-  go<int8_t>();
+  go<uint8_t,128,8,4>();
+  go<uint16_t,313/2,9,4>();
+
 
   return 0;
 }
