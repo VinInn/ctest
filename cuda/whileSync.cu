d8dadc3cdfb32610f35c9d8c1d036685701335eb
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-09-26 13:49:32 +0200

    works

--- cuda/whileSync.cu
+++ cuda/whileSync.cu
@@ -5,21 +5,29 @@ __global__
 void perm(int pad[]) {
     int t = threadIdx.x;
     int n=0;
-    __shared__ bool done;
 
     pad[2] =0;
-
-    done=true;
+    pad[3+threadIdx.x] = t;
+    pad[260]=0;
     __syncthreads();
-    while (done) {
-      done = false;
-      __syncthreads();
-      if ( t == 1 ) {
-        ++n;
-        pad[0] = 112211;
-        if (n<10) done = true;
+
+    bool more=true;
+    while (__syncthreads_or(more)) {
+      more = false;
+      for (int j=0; j<threadIdx.x; ++j) {
+        // if (3==t%4) { pad[3+threadIdx.x]++; }
+        if ( t == 5 ) {
+          ++n;
+          pad[0] = 112211;
+          if (n<20) more = true;
+        }
+        if (2==t%2) {
+          auto old = atomicMin(&pad[3+j],pad[3+threadIdx.x]);
+          if(old!=pad[3+threadIdx.x]) more=true;
+          atomicMin(&pad[3+threadIdx.x],old);
+        }
       }
-     __syncthreads();
+      if(0==threadIdx.x) ++pad[260];
     }
 
     __syncthreads();
@@ -36,7 +44,7 @@ void perm(int pad[]) {
 
 int main() {
 
-    int h_pad[3];
+    int h_pad[3+256+4];
     int *dev_pad = 0;
     cudaMalloc(&dev_pad, sizeof(h_pad));
     cudaMemset(dev_pad, 0, sizeof(h_pad));
@@ -46,7 +54,8 @@ int main() {
     cudaMemcpy(h_pad, dev_pad, sizeof(h_pad), cudaMemcpyDeviceToHost);
 
 
-    printf("pad[0] = %d    pad[1] = %d  pad[2] = %d\n", h_pad[0], h_pad[1], h_pad[2]);
+    printf("pad[0] = %d    pad[1] = %d  pad[2] = %d pad[last] = %d \n", h_pad[0], h_pad[1], h_pad[2], h_pad[2+256]);
+    printf("loops %d\n",h_pad[260]); 
     return 0;
 
 }
