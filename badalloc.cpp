904a0d971ac8a1b88c8093a80d7c83f5584ca85a
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-09-30 19:08:51 +0200

    add move

--- badalloc.cpp
+++ badalloc.cpp
@@ -2,6 +2,8 @@
 #include<algorithm>
 #include<iostream>
 namespace {
+  int nm = 0;
+  int nma = 0;
   int ns = 0;
   int nis = 0;
   int nd=0;
@@ -10,6 +12,8 @@ namespace {
 }
 
 void zero () {
+  nm = 0;
+  nma = 0;
   ns = 0;
   nis = 0;
   nd=0;
@@ -23,11 +27,19 @@ struct A {
   A() : i(0), v(10000){++nd;}
   explicit A(int ii, int s=10000) : i(ii), v(s){}
   A(const A& a) : i(a.i), v(a.v) {++nc;}
+  A(const A&& a) noexcept : i(a.i), v(std::move(a.v)) {++nm;}
   A& operator=(A const & a) {
     i=a.i; v=a.v; 
     ++na;
     return *this;
   }
+
+  A& operator=(A && a) noexcept {
+    i=a.i; v=std::move(a.v);
+    ++nma;
+    return *this;
+  }
+
 
   inline void swap(A&a) {
     std::swap(i,a.i);
@@ -45,10 +57,12 @@ inline bool operator<(A const& rh, A const& lh) {
 
 int main() {
 
+  int N=400000;
+
   std::vector<A> a;
   int i=0;
   try {
-    for(;;++i) 
+    for(;i<N;++i) 
       a.push_back(A(i));
   }
   catch(...) {
@@ -58,6 +72,7 @@ int main() {
   std::cout << "size " << a.size() << std::endl;
   std::cout << "number of swaps " << ns << " " << nis << std::endl;
   std::cout << "number of copy " << nc << " " << na << " " << nd << std::endl;
+  std::cout << "number of move " << nm << " " << nma  << std::endl;
 
   return 0;
 
--- cuda/atom.cu
+++ cuda/atom.cu
@@ -1,3 +1,9 @@
+#if __CUDA_ARCH__ < 600
+  #define atomicMin_block(X,Y) atomicMin(X,Y)
+  #define atomicAdd_block(X,Y) atomicAdd(X,Y)
+#endif
+
+
 __global__
 void glo(int * x, int * y) {
    atomicAdd(x+3,1);
