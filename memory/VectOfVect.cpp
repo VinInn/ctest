c810056ce67c7d4fbafaea7a922c04900f81954a
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-10-04 16:12:48 +0200

    fake better the indices

--- memory/VectOfVect.cpp
+++ memory/VectOfVect.cpp
@@ -115,23 +115,38 @@ void one(bool doprint) {
   // make na even (like is easear)
   na = 2*(na/2+1);
   int nb[na];
-  for(int i=0;i<na;++i) nb[i]=bGen(reng);
+  int totElement=0;
+  for(int i=0;i<na;++i) totElement += (nb[i]=bGen(reng));
   try {
     stop("before first loop");
     // here we fake a clustering process
-    // first we assume some how in each A associate twice as much elements
+    // WHAT WE KNOW IS ONLY THAT WE HAVE totElement ELEMENTS
+    // they are store somewhere we do not deal with the real algo here, only data structure
+    // representing their grouping
+    // we assume that the final results is "na" groups each associated with nb[i] elements.
+    // the solution can be any data structure not necessarely the "vector of vector of indices used here
+    // the only fixed assumpition is that in the first loop somehow each A is associated to twice as much elements
+    // the asserts shall be respected
     auto nah = na/2;
+    auto kk1=0;  // fakes the index (pointer) to an element
+    auto kk2=0; for(int i=0;i<nah;++i) kk2+=nb[i]; // same for the other half
     for(int i=0;i<nah;++i) {
       va.push_back(A(i)); 
       auto & v = va.back().v;
-      for(int j=0;j<nb[i];++j) v.push_back(j);
-      assert(i+nah<na);
-      for(int j=0;j<nb[i+nah];++j) v.push_back(j);
-      assert(v.size()==nb[i]+nb[i+nah]);
+      for(int j=0;j<nb[i];++j) v.push_back(kk1++);
+      for(int j=0;j<nb[i+nah];++j) v.push_back(kk2++);
+
     }
-    assert(va.back().i==nah-1);
+    stop("after first loop");
+ 
+    assert(va.size()==nah);
+    for(int i=0;i<nah;++i) {
+      assert(va[i].v.size()==nb[i]+nb[i+nah]);
+    }
+
     stop("before second loop");
     // now we split
+    // again algo irrelevant, just data structure and their filling to be optimized
     for(int i=0;i<nah;++i) {
       va.push_back(A(i+nah));
       auto & v1 = va[i].v;
@@ -143,19 +158,22 @@ void one(bool doprint) {
     }
     stop("after second loop");
 
+    int kk=0;
     assert(va.size()==na);
     for(int i=0;i<na;++i) {
       assert(va[i].i==i);
       assert(va[i].v.size()==nb[i]);
+      for (auto e : va[i].v) { assert(e==kk); ++kk;}
       totsize+=va[i].v.size();
       totcapacity+=va[i].v.capacity();
     }
 
     /// bonus sort!
+    stop("before sort");
     std::sort(va.begin(),va.end(),
 	      [](auto const & a, auto const & b) { return a.v.size()<b.v.size();}
 	      );
-    
+    stop("after sort");
   }
   catch(...) {
     std::cout << "oops" << std::endl;    
