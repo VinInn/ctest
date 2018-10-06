6f20eac2f03b82bd2bcf215452635a61a7b18761
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-10-06 08:42:19 -0400

    no problem on volta

--- cuda/condAtomic.cu
+++ cuda/condAtomic.cu
@@ -6,19 +6,24 @@ __global__ void bar(int n) {
     int laneId = threadIdx.x & 0x1f;
     __shared__ int res1;
     __shared__ int res2;
-
-    res1= res2=0;
+    __shared__ int res3;
+    __shared__ int v[1024];
+    res1= res2 = res3=0;
     __syncthreads();
 
     for (auto i=threadIdx.x; i<n; i+=blockDim.x) {
       if (laneId%3==1) atomicMax(&res1,i%laneId);
       if (laneId%3==2) atomicAdd(&res2,i%laneId);
+      if (laneId%3==2) {
+        auto old = atomicAdd(&res3,1);
+        v[old] = i%laneId;
+      }  
     }
 
     __syncthreads();
 
-    if (threadIdx.x==0) printf("res %d %d\n", res1,res2); 
-
+    if (threadIdx.x==0) printf("res %d %d %d\n", res1,res2,res3); 
+    if (threadIdx.x==0) printf("v %d %d %d\n", v[0],v[res3/2],v[res3-1]);
 }
 
 
