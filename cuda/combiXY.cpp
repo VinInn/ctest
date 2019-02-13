3d26e13b366b4a8770ac2c8bb9f3880c3b5e84d0
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-02-13 10:00:31 +0100

    make kernel running in plain sequential c++

---
+++ cuda/combiXY.cpp
@@ -0,0 +1,64 @@
+#include "cudaCompat.h"
+#include "combiXY.h"
+
+
+
+#include<iostream>
+#include<memory>
+#include<vector>
+
+constexpr uint32_t NTOT = 1024*8;
+
+template<int STRIDE, int NTTOT>
+void go(uint32_t * c_d, float const * z_d, float const * w_d, uint32_t * nss_d) {
+#ifdef VERIFY
+  uint32_t counters[10];
+  cudaMemset(c_d,0,10*sizeof(uint32_t));
+#endif
+
+
+  nn<STRIDE,NTTOT>(c_d, z_d,w_d,nss_d,NTOT,0.1f);
+
+#ifdef VERIFY
+  memcpy(counters,c_d,10*sizeof(uint32_t));
+
+  std::cout << STRIDE << ' ' << NTOT;
+  for (int i=0; i<5; ++i) std::cout << ' ' << counters[i];
+  std::cout << std::endl;
+#endif
+}
+
+
+int main() {
+
+
+  auto z_d = std::make_unique<float[]>(NTOT);
+  auto w_d = std::make_unique<float[]>(NTOT);
+  auto nns_d = std::make_unique<uint32_t[]>(NTOT);
+  auto c_d = std::make_unique<uint32_t[]>(10);
+
+  for (int i=0; i<16; ++i) {
+
+  cudaMemset(nns_d.get(),0,NTOT*sizeof(uint32_t));
+
+  std::vector<float> z_h(NTOT);  // for "unclear" reasons this is now zeroed...
+  std::mt19937 reng;
+  std::uniform_real_distribution<float> rgen(-1.,1.);
+
+  for (auto & z : z_h) z = rgen(reng);
+  memcpy(z_d.get(),z_h.data(),sizeof(float)*z_h.size());
+  for (auto & z : z_h) z = rgen(reng);
+  memcpy(w_d.get(),z_h.data(),sizeof(float)*z_h.size());
+
+
+  go<1,64>(c_d.get(), z_d.get(),w_d.get(),nns_d.get());
+
+  go<1,256>(c_d.get(), z_d.get(),w_d.get(),nns_d.get());
+ 
+ 
+  }
+  cudaDeviceSynchronize();
+
+  return 0;
+
+}
---
+++ cuda/combiXY.h
@@ -0,0 +1,49 @@
+#include<cassert>
+#include<cstdint>
+#include<cmath>
+#include<random>
+#include<vector>
+
+// #define VERIFY
+
+#ifdef VERIFY
+#define COUNT(x) atomicAdd(&counters[x],1);
+#else
+__device__
+void dummy(int){}
+#define COUNT(x) dummy(x);
+#endif
+
+template<int STRIDE, int NTTOT>
+__global__
+void nn(uint32_t * __restrict__ counters,
+float const * __restrict__ z, float const * __restrict__ w, uint32_t * __restrict__ nns, int ntot, float eps) {
+    COUNT(0);
+    // this part is actually run blockDim.x times for each "z"
+    auto id = blockIdx.y * blockDim.y + threadIdx.y;
+    auto first = threadIdx.x;
+    assert(blockDim.x==STRIDE);
+    assert(blockIdx.x==0);
+    assert(first<STRIDE);
+    // usual loop uder the assumption ntot is not kown on HOST side
+    auto incr = (blockDim.y * gridDim.y);
+    for (auto j = id; j < ntot; j += incr) {
+      COUNT(1)
+      // combinatorial loop  (n^2)
+      // in reality it should be limited using a Histogram, KDTree or similar
+      // here we parallelize. for each "z[j]" blockDim.x threads are actually used
+      auto k = j+ 1+first;
+      for (;k < ntot; k +=blockDim.x) {
+        COUNT(2);
+        if (
+             fabs(z[j]-z[k]) < eps && 
+             fabs(w[j]-w[k]) < eps
+           ) {
+          atomicAdd(&nns[j],1);
+          atomicAdd(&nns[k],1);
+          COUNT(3);
+        }
+      }  // inner loop k
+    } // outer loop j
+
+}
