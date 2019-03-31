35bab2cfe9d2679f5a89959f69534f917f3cbf01
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-03-31 19:53:41 +0200

    full device

--- cuda/prefixScan.h
+++ cuda/prefixScan.h
@@ -3,11 +3,34 @@
 
 #include <cstdint>
 
-#include <cassert>
+
+#include "HeterogeneousCore/CUDAUtilities/interface/cudaCompat.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/cuda_assert.h"
+
+
 #ifdef __CUDA_ARCH__
 template<typename T>
 __device__
 void 
+__forceinline__
+warpPrefixScan(T const * __restrict__ ci, T * __restrict__ co, uint32_t i, uint32_t mask) {
+   // ci and co may be the same
+   auto x = ci[i];
+   auto laneId = threadIdx.x & 0x1f;
+   #pragma unroll
+   for( int offset = 1 ; offset < 32 ; offset <<= 1 ) {
+     auto y = __shfl_up_sync(mask,x, offset);
+     if(laneId >= offset) x += y;
+   }
+   co[i] = x;
+}
+#endif
+
+//same as above may remove
+#ifdef __CUDA_ARCH__
+template<typename T>
+__device__
+void
 __forceinline__
 warpPrefixScan(T * c, uint32_t i, uint32_t mask) {
    auto x = c[i];
@@ -20,13 +43,62 @@ warpPrefixScan(T * c, uint32_t i, uint32_t mask) {
    c[i] = x;
 }
 #endif
+
+
+
 // limited to 32*32 elements....
 template<typename T>
 __device__ __host__
 void
 __forceinline__
-blockPrefixScan(T * c, uint32_t size, T* ws) {
+blockPrefixScan(T const * __restrict__ ci, T * __restrict__ co, uint32_t size, T* ws
+#ifndef __CUDA_ARCH__
+                =nullptr
+#endif
+               ) {
 #ifdef __CUDA_ARCH__
+  assert(ws);
+  assert(size<=1024);
+  assert(0==blockDim.x%32);
+  auto first = threadIdx.x;
+  auto mask = __ballot_sync(0xffffffff,first<size);
+
+  for (auto i=first; i<size; i+=blockDim.x) {
+    warpPrefixScan(ci,co,i,mask);
+    auto laneId = threadIdx.x & 0x1f;
+    auto warpId = i/32;
+    assert(warpId<32);
+    if (31==laneId) ws[warpId]=co[i];
+    mask = __ballot_sync(mask,i+blockDim.x<size);
+  }
+  __syncthreads();
+  if (size<=32) return;
+  if (threadIdx.x<32) warpPrefixScan(ws,threadIdx.x,0xffffffff);
+  __syncthreads();
+  for (auto i=first+32; i<size; i+=blockDim.x) {
+    auto warpId = i/32;
+    co[i]+=ws[warpId-1];
+  }
+  __syncthreads();
+#else
+  co[0]=ci[0];
+  for(uint32_t i=1; i<size; ++i) co[i]=ci[i]+co[i-1];
+#endif
+}
+
+// same as above, may remove
+// limited to 32*32 elements....
+template<typename T>
+__device__ __host__
+void
+__forceinline__
+blockPrefixScan(T * c, uint32_t size, T* ws
+#ifndef __CUDA_ARCH__
+                =nullptr
+#endif
+               ) {
+#ifdef __CUDA_ARCH__
+  assert(ws);
   assert(size<=1024);
   assert(0==blockDim.x%32);
   auto first = threadIdx.x;
@@ -54,5 +126,41 @@ blockPrefixScan(T * c, uint32_t size, T* ws) {
 #endif
 }
 
+
+// limited to 1024*1024 elements....
+template<typename T>
+__global__
+void
+multiBlockPrefixScan(T const * __restrict__ ci, T * __restrict__ co, int32_t size, int32_t * pc) {
+
+   __shared__ T ws[32];
+   // first each block does a scan of size 1024;
+  int off = 1024*blockIdx.x; 
+  if (size-off >0) blockPrefixScan(ci+off,co+off,std::min(1024,size-off),ws);
+
+  if (0==threadIdx.x) atomicAdd(pc,1);  // block counter
+   if (blockIdx.x>0) return;
+
+   if (0==threadIdx.x) while((*c)<gridDim.x) {}
+   __syncthreads();
+ 
+   // good each block has done its work and now we are left in block 0
+
+   // let's get the partial sums from each block
+   __shared__ T psum[1024];
+   for (int i=threadIdx.x; i<gridDim.x; i+=blockDim.x) {
+     auto j = 1024*i+1023;
+     psum[i] = (j<size) ? co[j] : 0;
+   }
+   __syncthreads();
+   blockPrefixScan(psum,psum,gridDim.x,ws);
+   
+   // now it would have been handy to have the other block around...
+   for (int i=first+1024; i<size; i+=blockDim.x) {
+       auto k = i/1024; // block
+       co[i]+=psum[k-1];     
+   }
+
+}
 
 #endif
