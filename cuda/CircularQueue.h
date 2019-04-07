96d7ff97d30beebf2b8b77e0f2346ed18c2a559f
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-04-07 18:55:21 +0200

    bah

--- cuda/CircularQueue.h
+++ cuda/CircularQueue.h
@@ -11,6 +11,18 @@ template<typename T, int MAXSIZE>
 class CircularQueue {
 public:
 
+  using ull_t = unsigned long long;
+
+  struct HeadTail {
+    uint32_t m_head;
+    uint32_t m_tail;
+  };
+
+  static constexpr ull_t one() { return ull_t(1)<<32;}
+  static constexpr uint32_t head(ull_t ht) { return ((HeadTail&)(ht)).m_head;}
+  static constexpr uint32_t tail(ull_t ht) { return ((HeadTail&)(ht)).m_tail;}
+
+
 #ifdef TEST_CIRCULAR_QUEUE
   static constexpr uint32_t maxCapacity = 8*1024;
   static constexpr uint32_t maxHead =     16*1024-1;
@@ -23,79 +35,97 @@ public:
   __device__
   bool constructEmpty(uint32_t capacity) {
    m_capacity = capacity;
-   m_tail=m_head=0;
+   tail()=head()=0;
    return m_capacity<=MAXSIZE;
   }
 
   __device__
   bool constructFull(uint32_t capacity) {
    m_capacity = capacity;
-   m_tail=0;
-   m_head=m_capacity;
+   tail()=0;
+   head()=m_capacity;
    return m_capacity<=MAXSIZE;
   }
 
-  // back to minimal range so that m_tail <=m_head
+  // back to minimal range so that tail <= head
   __device__
   void reset() {
-    auto old = m_head;
-    while (old==atomicCAS(&m_head,old,m_head&mask2())) {old = m_head;}
-    // now pop may fail
-    old = m_tail;
-    while (old==atomicCAS(&m_tail,old,m_tail&mask2())) {old = m_tail;}
+    auto old = m_ht;
+    auto val = old;
+    int s = int(val.m_head)-int(val.m_tail);
+    val.m_tail &= mask();
+    val.m_head = val.m_tail +s;
+
+    while (head()>maxHead && ((ull_t const&)old)!=atomicCAS(headTail(),(ull_t const&)old,(ull_t const&)val)) {
+      old = m_ht;
+      auto val = old;
+      int s = int(val.m_head)-int(val.m_tail);
+      val.m_tail &= mask();
+      val.m_head = val.m_tail +s;
+    }
   }
 
 
   // we assume we push only what already pop, overflow not possible
   __device__
   void unsafePush(const T &element) {
-    auto previous = atomicAdd(&m_head, 1);
+    auto previous = head(atomicAdd(headTail(), 1));
     previous &= mask();
     m_data[previous] = element;
-    if (m_head>maxHead) reset(); // avoid wrapping
+    if (head()>maxHead) reset(); // avoid wrapping
   }
 
   __device__
   bool push(const T &element) {
-    auto previous = atomicAdd(&m_head, 1);
-    if (previous-m_tail<capacity()) {
+    int t = tail();
+    auto previous = head(atomicAdd(headTail(), 1));
+    if (int(previous)-t < int(capacity())) {
       previous &= mask();
       m_data[previous] = element;
-      if (m_head>maxHead) reset(); // avoid wrapping
+//      if (head()>maxHead) reset(); // avoid wrapping
       return true;
     } else {
-      atomicSub(&m_head, 1);
+      atomicAdd(headTail(), -(1UL));
       return false;
     }
   }
 
   __device__
-  T pop(T invalid) {
-    auto previous = atomicAdd(&m_tail, 1);
-    if (previous<m_head) { 
+  T pop(T const invalid) {
+    auto h = head();
+    auto previous = tail(atomicAdd(headTail(), one()));
+    if (previous<h) { 
       previous &= mask();
-      return m_data[previous];
+      auto val = m_data[previous];
+//      while(invalid==val) {val = m_data[previous];}
+      m_data[previous] = invalid;
+      return val;
     } else {
-      atomicSub(&m_tail, 1);
+      atomicAdd(headTail(), -one());
       return invalid;
     }
   }
 
-  inline constexpr int size() const { return m_head-m_tail;}
-  inline constexpr bool empty() const { return m_tail>=m_head;}
-  inline constexpr bool full()  const { return size()>=capacity();}
+  inline constexpr int size() const { return int(head())-int(tail());}
+  inline constexpr bool empty() const { return tail()>=head();}
+  inline constexpr bool full()  const { return size()>=int(capacity());}
   inline constexpr uint32_t capacity() const { return m_capacity; }
   inline constexpr T const * data() const { return m_data; }
   inline constexpr T * data()  { return m_data; }
-  inline constexpr uint32_t mask() const { return m_capacity-1;}
-  inline constexpr uint32_t mask2() const { return 2*m_capacity-1;}
 
-  inline constexpr uint32_t head() const { return m_head;}
-  inline constexpr uint32_t tail() const { return m_tail;}
+  inline constexpr uint32_t mask() const { return m_capacity-1;}
+  inline constexpr ull_t mask2() const { return ull_t(m_capacity-1)<<32 | ull_t(m_capacity-1);}
+
+  inline constexpr uint32_t head() const { return m_ht.m_head;}
+  inline constexpr uint32_t tail() const { return m_ht.m_tail;}
+  inline constexpr uint32_t & head()  { return m_ht.m_head;}
+  inline constexpr uint32_t & tail() { return m_ht.m_tail;}
+
+  inline constexpr ull_t * headTail() { return (ull_t *)(&m_ht);}
+ 
 private:
 
-  uint32_t m_head;
-  uint32_t m_tail;
+  HeadTail m_ht;
   uint32_t m_capacity;  // must be power of 2
 
   T m_data[MAXSIZE];
--- cuda/CircularQueue_t.cu
+++ cuda/CircularQueue_t.cu
@@ -3,6 +3,11 @@
 #include<cassert>
 #include <cstdio>
 
+
+#define GPU_DEBUG
+#include "radixSort.h"
+
+
 __global__
 void test0() {
   
@@ -10,14 +15,19 @@ void test0() {
 
   __shared__ Q q;
   assert(q.constructEmpty(1024));
+  printf("empty ht %d %d\n",*q.headTail());
   assert(q.empty());
   assert(!q.full());
   assert(q.size()==0);
   assert(q.capacity()==1024);
+  assert(q.mask()==1023);
+  assert(q.head()==0);
+  assert(q.tail()==0);
   assert(-1==q.pop(-1));
   assert(q.empty());
 
   assert(q.push(1));
+  printf("one ht %d %d\n",*q.headTail());
   assert(!q.empty());
   assert(!q.full());
   assert(q.size()==1);
@@ -33,10 +43,14 @@ void test0() {
 
 
   assert(q.constructFull(1024));
+  for (int j=0;j<1024;++j) q.data()[j]=j;
+  printf("full ht %d %d\n",*q.headTail());
   assert(!q.empty());
   assert(q.full());
   assert(q.size()==1024);
   assert(q.capacity()==1024);
+  assert(q.head()==1024);
+  assert(q.tail()==0);
   assert(!q.push(1));
   assert(q.full());
   q.pop(-1);
@@ -51,7 +65,21 @@ void test0() {
   assert(!q.push(1));
   assert(q.full());
 
+
+  printf("ht %d %d\n",*q.headTail());
+
+  auto const h1 = q.head();
+  auto const t1 = q.tail();
+  printf("h,t s %d %d %d\n",q.head(),q.tail(),q.size());
+  q.reset();
+  printf("h,t s %d %d %d\n",q.head(),q.tail(),q.size());
+  assert(h1 == q.head());
+  assert(t1 == q.tail());
+
+
+
 }
+
 
 __global__
 void testSimple() {
@@ -86,7 +114,9 @@ void testSimple() {
   if (0==i) printf("h,t %d %d\n",q.head(),q.tail());
   __syncthreads();
 
+  /*
   k = q.pop(-1);
+  assert(k>=0);
   if (k%2) assert(q.push(k));
   __syncthreads();
   assert(!q.full());
@@ -104,6 +134,7 @@ void testSimple() {
   __syncthreads();
   assert(q.full());
   __syncthreads();
+  */
 
   k = q.pop(-1);
   assert(k>=0); assert(k<1024);
@@ -117,12 +148,80 @@ void testSimple() {
   if (k>0) assert(k%2);
   __syncthreads();
   assert(q.empty());
-  if (0==i) printf("h,t s %d %d %d\n",q.head(),q.tail(),q.size());
+  if (0==i) {
+    printf("h,t s %d %d %d\n",q.head(),q.tail(),q.size());
+    printf("ht %d %d\n",*q.headTail());
+  }
   __syncthreads();
 
+  int jj=0;
+  while(__syncthreads_or(jj<16)) {
+    if (jj>=7 && 0==i) {
+      printf("jj %d: h,t s %d %d %d ",jj,q.head(),q.tail(),q.size()); 
+      printf("ht %d %d\n",*q.headTail());
+    }
+    k = q.pop(-1);
+    if (k>=0 && k%2) q.push(k);
+    q.push(jj);
+    ++jj;
+  }
 
+  // just a mess
+  for (int kk=0; kk<32; ++kk) {
+    k = q.pop(-1);
+    if (k>=0 && k%2) q.unsafePush(k);
+    if (k>=0 && k%3) q.push(kk);
+  }
+  __syncthreads();
+  if (0==i) {
+    printf("h,t s %d %d %d\n",q.head(),q.tail(),q.size());
+    printf("ht %d %d\n",*q.headTail());
 
+    q.constructFull(1024);
+  }
+
+  q.data()[i]=i;
+  __shared__ int v[1024];
+  __shared__ int vc;
+  v[i]=-2;
+  vc=0;
+  __syncthreads();
+
+  assert(q.full());
+  assert(0==q.data()[0]); 
+  assert(1023==q.data()[1023]);
+  __syncthreads();
+
+  for (int kk=0; kk<32; ++kk) {
+    k = q.pop(-1);
+    if (k<0) continue;
+    if ((k+kk)%2) q.unsafePush(k);
+    else {
+      auto j = atomicAdd(&vc,1);
+      assert(j<1024);
+      v[j]=k;
+    }
+  }
+
+  __syncthreads();
+  if (0==i) printf("filled %d\n",vc);
+  if (0==i) {
+    printf("h,t s %d %d %d\n",q.head(),q.tail(),q.size());
+  }
+  __shared__ uint16_t sortInd[1024];
+  __shared__ uint16_t sws[1024];
+  radixSort(v,sortInd,sws,vc);
+  __syncthreads();
+  if (0==i) printf("sorted %d %d\n",v[sortInd[0]],v[sortInd[vc-1]]);
+  assert(v[i]!=-1);
+  if (i<vc) { assert(v[i]>=0); assert(v[i]<1024); }
+  if(i>0 && i<vc) if (!(v[sortInd[i-1]]<v[sortInd[i]])) 
+         printf("mess %d %d %d %d\n",sortInd[i-1],sortInd[i],v[sortInd[i-1]],v[sortInd[i]]);
+ 
 }
+
+
+
 
 
 #include<iostream>
@@ -133,8 +232,10 @@ int main() {
 
    std::cout << "Qmaxs " << Q::maxCapacity << ' ' << Q::maxHead << std::endl;
 
+   std::cout << "\ntest0" << std::endl;
    test0<<<1,1,0>>>();   
    cudaDeviceSynchronize();
+   std::cout << "\ntestSimple" << std::endl;
    testSimple<<<1,1024,0>>>();
    cudaDeviceSynchronize();
 
