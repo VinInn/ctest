12c910b89eee86068926f3433c4e55ca062936e3
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-09-24 13:58:04 +0200

    extend test, fix copy

--- SmartVectorUnion.cpp
+++ SmartVectorUnion.cpp
@@ -40,16 +40,19 @@ public :
    if(m_isArray) {
     m_container.a=sm.m_container.a;
    } else {
+    std::fill(m_container.a.begin(), m_container.a.end(),0);
     m_container.v=sm.m_container.v;
    }
   }
   SmartVector& operator=(const SmartVector& sm) {
-    m_isArray = sm.m_isArray;
-   if(m_isArray) {
+   if(!m_isArray) m_container.v.~Vector();
+   if(sm.m_isArray) {
     m_container.a=sm.m_container.a;
    } else {
+    std::fill(m_container.a.begin(), m_container.a.end(),0);
     m_container.v=sm.m_container.v;
    }
+   m_isArray = sm.m_isArray;
    return *this;
   }
 
@@ -57,17 +60,20 @@ public :
    if(m_isArray) {
     m_container.a=std::move(sm.m_container.a);
    } else {
+    std::fill(m_container.a.begin(), m_container.a.end(),0);
     m_container.v=std::move(sm.m_container.v);
    }
   }
 
   SmartVector& operator=(SmartVector&& sm) {
-    m_isArray = sm.m_isArray;
-   if(m_isArray) {
+   if(!m_isArray) m_container.v.~Vector();
+   if(sm.m_isArray) {
     m_container.a=std::move(sm.m_container.a);
    } else {
+    if(m_isArray) std::fill(m_container.a.begin(), m_container.a.end(),0);
     m_container.v=std::move(sm.m_container.v);
    }
+   m_isArray = sm.m_isArray;
    return *this;
   }
 
@@ -146,12 +152,13 @@ private:
   bool m_isArray = true;
 };
 
-int main() {
 
- using Vector = std::vector<uint8_t>;
- using Array = std::array<uint8_t,sizeof(Vector)/sizeof(uint8_t)>;
- using Variant = SmartVector<uint8_t>;
+template<typename T>
+int go() {
 
+ using Vector = std::vector<T>;
+ using Array = std::array<T,sizeof(Vector)/sizeof(T)>;
+ using Variant = SmartVector<T>;
 
  std::cout << sizeof(Vector) <<' '<< sizeof(Array) <<' '<< sizeof(Variant) << std::endl;
 
@@ -161,15 +168,23 @@ int main() {
  assert(0==v.size());
 }
 
- uint8_t data[128];
+ T data[128];
  for (int i=0; i<128; ++i) data[i]=i;
 
- uint8_t i=0;
+ T i=0;
  Variant va(data,data+5);
  assert(5==va.size());
  assert(5==va.end()-va.begin());
  assert(va.isArray());
  i=0; for (auto c : va) assert(c==i++);
+
+ auto vc = va;
+ assert(5==vc.size());
+ assert(5==vc.end()-vc.begin());
+ assert(vc.isArray());
+ i=0; for (auto c : vc) assert(c==i++);
+
+
 {
  Variant vb; vb.initialize(data,data+24);
  assert(24==vb.size());
@@ -186,7 +201,10 @@ int main() {
  va.extend(data+5,data+10);
  assert(10==va.size());
  assert(10==va.end()-va.begin());
- assert(va.isArray());
+ if constexpr (sizeof(T)<4)
+    assert(va.isArray());
+ else
+    assert(!va.isArray());
  i=0; for (auto c : va) assert(c==i++);
  va.extend(data+10,data+64);
  assert(64==va.size());
@@ -197,11 +215,26 @@ int main() {
  vv.extend(data+64,data+72); 
  assert(72==vv.size());
  assert(72==vv.end()-vv.begin());
- assert(!va.isArray());
+ assert(!vv.isArray());
  i=0; for (auto c : vv) assert(c==i++);
+
+ vc = vv;
+ assert(72==vc.size());
+ assert(72==vc.end()-vc.begin());
+ assert(!vc.isArray());
+ i=0; for (auto c : vc) assert(c==i++);
+
 
  return 0;
 
 }
 
 
+int main() {
+
+  go<uint8_t>();
+  go<uint16_t>();
+  go<int32_t>();
+
+  return 0;
+}
