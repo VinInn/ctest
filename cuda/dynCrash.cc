4273ee92369bb54ba8b3a8972201b78453d2bf4e
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-12-21 11:16:56 +0100

    more tests all ok

--- cuda/dynCrash.cc
+++ cuda/dynCrash.cc
@@ -1,14 +1,29 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include<cassert>
+struct Go {
+
+  virtual ~Go(){}
+
+  virtual void go() =0;
+
+  inline
+  static Go * me(Go * in=nullptr) {
+    static Go * l = nullptr;
+    if (in) l=in;
+    return l;
+  }
+
+};
 
 
 #include "cudaCheck.h"
 
-void alloc() {
-  int a[10]; a[0]=4;
+void alloc(int i) {
+  int * a = new int[i]; a[0]=4;
   int * d;
-  cudaMalloc(&d,40);
-  cudaMemcpyAsync(d,a,40,cudaMemcpyHostToDevice,0);
+  cudaMalloc(&d,4*i);
+  cudaMemcpyAsync(d,a,4*i,cudaMemcpyHostToDevice,0);
   cudaCheck(cudaGetLastError());
   cudaDeviceSynchronize();
   cudaCheck(cudaGetLastError());
@@ -16,12 +31,16 @@ void alloc() {
 
 
 #include<iostream>
-struct Me2 {
+struct Me2 : private Go {
 
   Me2() {
-   std::cout << "Loaded" << std::endl;
-   alloc();
+   std::cout << "Loaded Me2" << std::endl;
+   alloc(1024);
+   // assert(this==me(this));
   }
+
+
+  void go() override { alloc(1024);    std::cout << "Me2 go" << std::endl; }
 
 };
 
--- cuda/dynCrash.cu
+++ cuda/dynCrash.cu
@@ -1,15 +1,43 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include<cassert>
+struct Go {
 
+  virtual ~Go(){}
+
+  virtual void go() =0;
+  
+  inline
+  static Go * me(Go * in=nullptr) {
+    static Go * l = nullptr;
+    if (in) l=in;
+    return l;
+  }
+
+};
 
 #include<cstdio>
+
+ __device__ uint32_t hashedIndexEE(uint32_t id);
+
+__constant__ int q[] = {0,1,2,3,4};
 
 __global__
 void bar(int * i) {
   extern __shared__  unsigned char shared_mem[];
-  shared_mem[threadIdx.x]=i[threadIdx.x];
+  shared_mem[threadIdx.x]=i[threadIdx.x]*q[2]*hashedIndexEE(i[7]);
   __syncthreads();
-  printf("bar %d\n", shared_mem[0]);
+  printf("bar %d %d\n", shared_mem[0],q[3]);
+  int * q = 0;
+  if (i[2]>3) {
+    q = new int[i[2]];
+    memset(q,0,4*i[2]);
+    atomicAdd(q+2,shared_mem[0]);
+    memcpy(i,q,min(40,4*i[2]));
+    __syncthreads();
+    i[4] = q[threadIdx.x];
+  }
+  delete [] q;
 }
 
 struct Large {
@@ -45,7 +73,7 @@ void crash() {
 
 #include "cudaCheck.h"
 void wrapper() {
-  int a[10]; a[0]=4;
+  int a[10]; a[0]=4; a[2]=0;
   int * d;
   cudaMalloc(&d,40);
   cudaMemcpyAsync(d,a,40,cudaMemcpyHostToDevice,0);
@@ -63,14 +91,31 @@ void wrapper() {
 
 
 #include<iostream>
-struct Me {
+struct Me : private Go {
 
-  Me() {
-   std::cout << "Loaded" << std::endl;
+  Me(int a) {
+     std::cout << "Loaded " << a << std::endl;
+     assert(this==Go::me(this));
+     assert(this==Go::me());
+  }
+
+  void go() override {
+   std::cout << "go" << std::endl;
    wrapper();
+   std::cout << "gone" << std::endl;
   }
 
 };
 
 
-Me me;
+Me me(3);
+
+struct QQ {
+  QQ() {
+    std::cerr << "QQ Loaded"<< std::endl;
+   }
+
+};
+
+
+QQ qq;
