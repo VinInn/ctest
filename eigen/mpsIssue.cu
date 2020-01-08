8d306490f3dcb5e745642e893bd7ac4a3ce0cfad
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2020-01-08 16:19:17 +0100

    minimal code

--- eigen/mpsIssue.cu
+++ eigen/mpsIssue.cu
@@ -1,37 +1,28 @@
-#include "inplace_fnnls.h"
+#include <Eigen/Dense>
 
-    __device__ __forceinline__ SampleVector::Scalar compute_chi2(SampleDecompLLT& covariance_decomposition,
-                                                                 PulseMatrix const& pulse_matrix,
-                                                                 SampleVector const& amplitudes,
-                                                                 SampleVector const& samples) {
-      return covariance_decomposition.matrixL().solve(pulse_matrix * amplitudes - samples).squaredNorm();
-    }
+    constexpr int SampleVectorSize = 10;
+    constexpr int FullSampleVectorSize = 19;
+    constexpr int PulseVectorSize = 12;
+    constexpr int NGains = 3;
 
+    using data_type = double;
+
+    typedef Eigen::Matrix<data_type, SampleVectorSize, 1> SampleVector;
+    typedef Eigen::Matrix<data_type, SampleVectorSize, SampleVectorSize> SampleMatrix;
+    typedef Eigen::Matrix<data_type, Eigen::Dynamic, Eigen::Dynamic, 0, PulseVectorSize, PulseVectorSize> PulseMatrix;
+    typedef Eigen::Matrix<char, SampleVectorSize, 1> BXVector;
+
+    typedef Eigen::LLT<SampleMatrix> SampleDecompLLT;
+    typedef Eigen::LLT<PulseMatrix> PulseDecompLLT;
+    typedef Eigen::LDLT<PulseMatrix> PulseDecompLDLT;
 
 __global__
-void go(SampleMatrix * inverse_cov, PulseMatrix * pulse_matrix, SampleVector * samples,  SampleVector * amplitudes, BXVector * bxs, int max_iterations)  {
-
-    int iter=0;
-    while (true) {
-          if (iter >= max_iterations)
-            break;
-
+void go(SampleMatrix * inverse_cov, PulseMatrix * pulse_matrix, SampleVector * samples)  {
     SampleDecompLLT covariance_decomposition;
     covariance_decomposition.compute(inverse_cov[threadIdx.x]);
 
     SampleMatrix A = covariance_decomposition.matrixL().solve(pulse_matrix[threadIdx.x]);
     SampleVector b = covariance_decomposition.matrixL().solve(samples[threadIdx.x]);
 
-     // samples[threadIdx.x] = A*b;
-     int npassive=0;
-     auto t = inplace_fnnls(A,b, amplitudes[threadIdx.x], npassive,bxs[threadIdx.x],pulse_matrix[threadIdx.x]);
-
-     if (!t) break;
-
-     auto chi2 = compute_chi2(covariance_decomposition,pulse_matrix[threadIdx.x],amplitudes[threadIdx.x],samples[threadIdx.x]);
-     
-     if (chi2<1.e-3) break;
-     ++iter;
-   } 
-
+    samples[threadIdx.x] = A*b;
 }
