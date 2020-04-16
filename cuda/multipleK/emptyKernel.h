b40c02d830a84a52c57708c1220c3e88d2ba6633
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2020-04-16 15:54:16 +0200

    test of kernel in multiple cus

---
+++ cuda/multipleK/emptyKernel.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "cuda.h"
+
+
+
+template <typename T>
+__global__ void EmptyKernel(void) { }
+
+
+inline __attribute__((always_inline)) cudaError_t PtxVersion(int &ptx_version)
+{
+    struct Dummy
+    {
+        /// Type definition of the EmptyKernel kernel entry point
+        typedef void (*EmptyKernelPtr)();
+
+        /// Force EmptyKernel<void> to be generated if this class is used
+        inline __attribute__((always_inline))
+        EmptyKernelPtr Empty()
+        {
+            return EmptyKernel<void>;
+        }
+    };
+
+    cudaError_t error = cudaSuccess;
+    cudaFuncAttributes empty_kernel_attrs;
+    error = cudaFuncGetAttributes(&empty_kernel_attrs, EmptyKernel<void>))) ;
+    if(!error) ptx_version = empty_kernel_attrs.ptxVersion * 10;
+
+    return error;
+}
