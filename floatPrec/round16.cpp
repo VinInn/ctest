1afbce23a474bc1d22cb947a31e080b68d143380
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2020-04-19 19:00:00 +0200

    round to precision N

---
+++ floatPrec/round16.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<limits>
+#include<cstdio>
+#include<cstring>
+
+template<typename T>
+void print(T x) {
+ std::cout<< std::hexfloat << x <<' '<<  std::scientific << std::setprecision(8) << x << ' ' <<  std::defaultfloat << x << std::endl;
+}
+
+
+
+template<int N> 
+float roundit(float x) {
+  static_assert(N<23);
+  constexpr auto shift = 23-N; 
+  constexpr uint32_t mask = 1<<(shift-1);
+
+  uint32_t i; memcpy(&i, &x, sizeof(x));
+
+  i += (i>=0) ? mask : -mask;
+  memcpy(&i, &x, sizeof(x));
+  i>>=shift; i<<=shift;
+  memcpy(&x, &i, sizeof(x));
+
+  return x;
+}
+
+
+
+int main() {
+
+
+  print(roundit<8>(M_PI));
+  print(roundit<12>(M_PI));
+  print(roundit<16>(M_PI));
+  print(roundit<20>(M_PI));
+  print(roundit<22>(M_PI));
+
+  print(roundit<8>(-M_PI));
+  print(roundit<12>(-M_PI));
+  print(roundit<16>(-M_PI));
+  print(roundit<20>(-M_PI));
+  print(roundit<22>(-M_PI));
+
+
+  return 0;
+}
