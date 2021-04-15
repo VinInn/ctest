9e23d9899bee68256c6a7e0a7d41a34fafff6b93
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2021-04-15 13:25:09 +0200

    add intrinsics

--- floatPrec/oneLineRecip.cpp
+++ floatPrec/oneLineRecip.cpp
@@ -1,9 +1,15 @@
 #include <cmath>
 #include<cstdio>
 
+#include <x86intrin.h>
+
 int main(int n, char * v[]) {
    float fn = n - 1.f;
    float k = 0.1f + fn;
+
+   float y;
+   _mm_store_ss( &y, _mm_rsqrt_ss( _mm_load_ss( &k ) ) );
+   printf("%a %a\n",k,y);
 
    float q = 1.f/std::sqrt(k);
    printf("%a %a\n",k,q);
