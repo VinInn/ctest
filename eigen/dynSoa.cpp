da8f2437cbd97e93f868e4072aff26fae272735e
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2021-07-08 14:39:49 +0200

    more complex

--- eigen/dynSoa.cpp
+++ eigen/dynSoa.cpp
@@ -10,14 +10,32 @@ using CStride = Eigen::InnerStride<1024>;
 using MapV3 =  Eigen::Map<V3,0, DynStride>;
 using CMapV3 =  Eigen::Map<const V3,0,  DynStride>;
 
+template<typename V>
+constexpr auto soit(float * __restrict__ v, int n, int i) {
+  return Eigen::Map<V,0, DynStride>(v+i, V::RowsAtCompileTime, V::ColsAtCompileTime, DynStride(n));
+}
+
+
+template<typename V>
+constexpr auto csoit(float const * __restrict__ v, int n, int i) {
+  return Eigen::Map<const V,0, DynStride>(v+i, V::RowsAtCompileTime, V::ColsAtCompileTime, DynStride(n));
+}
 
 #include<iostream>
-inline void cross(float const * __restrict__ v1, float const * __restrict__ v2, float * __restrict__ v3, int n) {
+
+
+void crossX(float const * __restrict__ v1, float const * __restrict__ v2, float * __restrict__ v3, int n) {
   for (int tid=0; tid<n; ++tid) {
     CMapV3 m1(v1+tid, V3::RowsAtCompileTime, V3::ColsAtCompileTime, DynStride(n));
     CMapV3 m2(v2+tid, V3::RowsAtCompileTime, V3::ColsAtCompileTime, DynStride(n));
     MapV3 m3(v3+tid, V3::RowsAtCompileTime, V3::ColsAtCompileTime, DynStride(n));
     m3 = m1.cross(m2);
+  }
+}
+
+void cross(float const * __restrict__ v1, float const * __restrict__ v2, float * __restrict__ v3, int n) {
+  for (int i=0; i<n; ++i) {
+    soit<V3>(v3,n,i) = csoit<V3>(v1,n,i).cross(csoit<V3>(v2,n,i));
   }
 }
 
