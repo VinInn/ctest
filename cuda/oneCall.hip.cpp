eaab8f6a6719c50449c48ccb8f3abcda505f2726
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2021-08-04 13:19:36 +0200

    for gamma

--- cuda/oneCall.hip.cpp
+++ cuda/oneCall.hip.cpp
@@ -3,23 +3,25 @@
 #include<cstdlib>
 
 __global__ void doit(float x) {
-   auto y =y1f(x);
+   auto y =log(x);
    printf ("float %a %a\n",x,y);
 }
 
 __global__ void doit(double x) {
-   auto y =y1(x);
-   printf ("double %a %a\n",x,y);
+   auto y =log(x);
+   auto z1= lgamma(x);
+   auto z2= lgamma(-x);
+   printf ("double %a %a %a %a\n",x,y,z1,z2);
 }
 
 
 int main() {
 
-  float f = -0.0f;
+  float f = 0x1.5efad5491a79bp-1022;
   hipLaunchKernelGGL(doit,dim3(1),dim3(1),0,0,f);
-  auto y =y1f(f);
+  auto y =log(f);
   printf ("cpu float %a %a\n",f,y);
-  double    d = -0.0f;
+  double    d =  0x1.p500*std::numeric_limits<double>::min();  //  0x1.0p-9; // 0x1.5efad5491a79bp-1022;
   hipLaunchKernelGGL(doit,dim3(1),dim3(1),0,0,d);
   hipStreamSynchronize(0);
 }
