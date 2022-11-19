edde19ff23dc928b6a52a39ee91a1ccdc23a62b6
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2022-11-19 15:41:24 +0100

    scimark

---
+++ scimark2/Random.h
@@ -0,0 +1,28 @@
+#ifdef HIDDEN
+#pragma GCC visibility push(hidden)
+#else
+#pragma GCC visibility push(default)
+#endif
+
+typedef struct
+{
+  int m[17];                        
+  int seed;                             
+  int i;                                /* originally = 4 */
+  int j;                                /* originally =  16 */
+  int /*boolean*/ haveRange;            /* = false; */
+  double left;                          /*= 0.0; */
+  double right;                         /* = 1.0; */
+  double width;                         /* = 1.0; */
+}
+Random_struct, *Random;
+
+Random new_Random_seed(int seed);
+double Random_nextDouble(Random R);
+void Random_delete(Random R);
+double *RandomVector(int N, Random R);
+double **RandomMatrix(int M, int N, Random R);
+
+#pragma GCC visibility pop
+
+
