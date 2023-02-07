19fb66ff4fb2708023aae0eb0ad7d71c9c53e132
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2023-02-07 10:14:30 +0100

    let the bug show up

--- floatPrec/sub.cpp
+++ floatPrec/sub.cpp
@@ -7,10 +7,10 @@ int main() {
 
 typedef union { unsigned int n; float x; } union_t;
 
-  _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_OFF);
-  _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_OFF);
+//  _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_OFF);
+//  _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_OFF);
 
-  #pragma omp parallel for schedule(dynamic,32)
+//  #pragma omp parallel for schedule(dynamic,32)
   for (int i=0; i<10; ++i) {
     union_t u;
     u.n = i;
