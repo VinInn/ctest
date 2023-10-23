70ea4500b99ceedf7c946c074ea9b77f2c09b041
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2023-10-23 15:26:44 +0200

    need compiler update

---
+++ getStacktrace.cc
@@ -0,0 +1,34 @@
+#include <stacktrace>
+
+  std::string get_stacktrace() {
+     std::string trace;
+     for (auto & entry : std::stacktrace::current() ) trace += entry.description() + '#';
+     return trace;
+  }
+
+
+#include <cstdlib>
+#include <malloc.h>
+#include <iostream>
+
+extern "C"
+void * myMallocHook(size_t size, void const * caller) {
+  __malloc_hook = nullptr;
+  auto p = malloc(size);
+  std::cout << "asked " << size
+            << " at " << get_stacktrace()
+            << std::endl;
+  __malloc_hook = myMallocHook;
+  return p;
+}
+
+namespace {
+struct Hook {
+  Hook() {
+  __malloc_hook = myMallocHook;
+  }
+};
+
+  Hook hook;
+
+}
