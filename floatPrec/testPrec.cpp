1900ab8d53c491af2dc61145cb47942254a4db9e
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2024-07-05 14:24:55 +0200

    more flex

--- floatPrec/testPrec.cpp
+++ floatPrec/testPrec.cpp
@@ -1,18 +1,24 @@
 #include<cstdio>
 
-using Float = double;
+#ifndef FLOAT
+#error please define FLOAT
+#endif
+
+using Float = FLOAT;
 
 int main() {
 
   const Float t = 1.;
-  Float r = 2;
+  Float r = 1;
   Float a = 1.;
   const Float h = 0.5;
-  while ( t!=r)  {
-    r = t + a;
+  while (a==r)  {
     a*=h;
+    r = (t + a) - t;
   } 
   printf("%f %a\n",a,a);
-
-  return 0;
+  printf("%f %a\n",r,r);
+  r = (__float128(t) + __float128(a)) - __float128(t);
+  printf("%f %a\n",r,r);
+  return a;
 }
