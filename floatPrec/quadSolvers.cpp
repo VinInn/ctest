81f003b596ac2c777f1051e2dce7ae4aa0f9fc87
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2024-07-10 18:24:57 +0200

    make it working in float128

--- floatPrec/quadSolvers.cpp
+++ floatPrec/quadSolvers.cpp
@@ -2,6 +2,8 @@
 #include<cmath>
 #include<tuple>
 #include<limits>
+#include<quadmath.h>
+
 
 template<typename T>
 inline T det(T a, T b, T c) {
@@ -9,13 +11,20 @@ inline T det(T a, T b, T c) {
   return std::sqrt(b*b-a*c);
 }
 
+template<>
+inline __float128 det(__float128 a,  __float128 b, __float128 c) {
+  // compute determinant for equation ax^2 + 2bx + c= 0
+  return ::sqrtq(b*b-a*c);
+}
+
+
 template<typename T>
 inline std::tuple<T,T> quadSolverNaive(T a, T b, T c) {
   // solve equation ax^2 + 2bx + c= 0
   // using naive solution (as at college)
   auto d = -T(1)/a;
   auto q = det(a,b,c);
-  return std::make_tuple(d*(b+q),d*(b-q));  
+  return std::make_tuple(d*(b-q),d*(b+q));  
 }
 
 
@@ -27,16 +36,25 @@ inline std::tuple<T,T> quadSolverOpt(T a, T b, T c) {
   return std::make_tuple(q/a,c/q);
 }
 
+template<>
+inline std::tuple<__float128,__float128> quadSolverOpt(__float128 a, __float128 b, __float128 c) {
+  // solve equation ax^2 + 2bx + c= 0
+  // using stable algorithm
+  auto q = -(::copysignq(det(a,b,c),b)+b);
+  return std::make_tuple(q/a,c/q);
+}
+
 
 
 
 #include<iostream>
 #include<iomanip>
 
+using LD = long double;
 
 template<typename T> 
 void print(T x) {
-  std::cout<< std::hexfloat << x <<' ' <<  std::scientific << std::setprecision(std::numeric_limits<T>::digits10+3) << x << std::endl;
+  std::cout<< std::hexfloat << x <<' ' <<  std::scientific << std::setprecision(std::numeric_limits<T>::digits10+3) << LD(x) << std::endl;
 }
 
 
@@ -47,9 +65,9 @@ void go(T a) {
   T b=-0.5*1.786737601482363, c=2.054360090947453e-8;
   auto s1 = quadSolverNaive(a,b,c);
   auto s2 = quadSolverOpt(a,b,c);
-  if (s1==s2) std::cout << "precise!!!" << std::endl;;
-  std::cout << "Naive Solution "<<  std::scientific << std::setprecision(std::numeric_limits<T>::digits10+3) << std::get<0>(s1) << ' ' << std::get<1>(s1) << std::endl;
-  std::cout <<  "Opt  Solution "<<  std::scientific << std::setprecision(std::numeric_limits<T>::digits10+3) << std::get<0>(s2) << ' ' << std::get<1>(s2) << std::endl;
+  if (s1==s2) std::cout << "precise!!!" << std::endl;
+  std::cout << "Naive Solution "<<  std::scientific << std::setprecision(std::numeric_limits<T>::digits10+3) << LD(std::get<0>(s1)) << ' ' << LD(std::get<1>(s1)) << std::endl;
+  std::cout << " Opt  Solution "<<  std::scientific << std::setprecision(std::numeric_limits<T>::digits10+3) << LD(std::get<0>(s2)) << ' ' << LD(std::get<1>(s2)) << std::endl;
   std::cout << std::endl;
 }
 
@@ -93,7 +111,7 @@ void circle(int q) {
 
 int main(int argc, char **){
 
-//   go<__float128>(argc);
+  go<__float128>(argc);
   go<double>(argc);
   go<float>(argc);
 
