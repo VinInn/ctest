5a4583d768a340af81c74c644aed639b63e8d508
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2024-10-04 18:27:45 +0200

    offline

--- floatPrec/cplxMul.cpp
+++ floatPrec/cplxMul.cpp
@@ -25,12 +25,22 @@ int main(int argc, char** argv)
 
 // ./a.out 0x1.6912cap-41f -0x1.69020ap-41f 0x1.d16ed4p-85f 0x1.6b497ep-110f
 // ./a.out 0x1.d16ed4p-85f 0x1.6b497ep-110f 0x1.6912cap-41f -0x1.69020ap-41f 
+
+std::complex<float> 
+__attribute__ ((noinline))
+mul(std::complex<float> const & a, std::complex<float> const & b)
+{
+   return a*b;
+}
+
 int main(int argc, char** argv)
 {
  std::complex<float> z1{atof(argv[1]),atof(argv[2])};
  std::complex<float> z2{atof(argv[3]),atof(argv[4])};
- std::complex<float> t1 = z1 * z2;
+ std::complex<float> t1 = z1*z2;
+ std::complex<float> t2 = mul(z1,z2);
  printf ("t1=(%a,%a)\n", t1.real(), t1.imag());
+ printf ("t2=(%a,%a)\n", t2.real(), t2.imag());
  return 0;
 }
 
