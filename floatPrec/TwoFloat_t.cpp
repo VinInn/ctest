e9dee06e101c6f7010bf8444d6ee55a6f33fa96c
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2025-08-28 17:37:12 +0200

    implement operators

--- floatPrec/TwoFloat_t.cpp
+++ floatPrec/TwoFloat_t.cpp
@@ -13,5 +13,13 @@ int main() {
   std::cout << std::hexfloat << double(f.hi())+double(f.lo())<< std::endl;
   std::cout << std::hexfloat << d.hi() << ',' << d.lo() << std::endl;
 
+  auto f1 = f;
+  TwoFloat<float> f2(1.e-3*std::sqrt(3.f),1.e-6*std::sqrt(2.f));
+  auto d1 = d;
+  double d2 = double(f2.hi())+double(f2.lo());
+  std::cout << std::hexfloat << f2.hi() << ',' << f2.lo() << std::endl;
+  std::cout << std::hexfloat << d2 << std::endl;
+
+
   return 0;
 }
