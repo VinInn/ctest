398a491d02949cf42855193ddd3d37599de2cd24
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2025-12-17 16:13:44 +0100

    more fastpow

--- floatPrec/fastpow.cpp
+++ floatPrec/fastpow.cpp
@@ -1,8 +1,10 @@
 #include <cmath>
 #include <cstdio>
 #include <iostream>
+#include <cstdint>
 
 
+// better b be positive and <1
 inline double fastPow(double a, double b) {
   union {
     double d;
@@ -13,17 +15,27 @@ inline double fastPow(double a, double b) {
   return u.d;
 }
 
-
+inline float fastPow(float a, float b) {
+  union {
+    float d;
+    uint32_t x;
+  } u = { a };
+  u.x = (uint32_t)(b * (u.x - 1064866808) + 1064866808);
+  return u.d;
+}
 
 int main() {
   constexpr double Safety = 0.9;
   double eps=1.e-5;
-  constexpr float cut = std::pow(10.f/Safety,5.f);
+  const float cut = std::pow(10.f/Safety,5.f);
   double accMin = eps/cut;
   std::cout << "eps/cut/accMin " << eps << " " << cut << " " << accMin
 	    << std::endl;
   for (double acc=accMin; acc<eps;  acc*=10.)
-    std::cout << acc << " " << std::pow(eps/acc,0.2f) << " " << fastPow(eps/acc,0.2f) << std::endl;
+    std::cout << acc << " " << std::pow(eps/acc,0.2) << " " << fastPow(eps/acc,0.2) << std::endl;
+
+  for (float x=0.5f; x<20.f; x+=0.5f)
+    std::cout << x << " " << std::pow(x,-1.0228f) << " " << fastPow(x,1.f-0.0228f)/x/x << ' ' << 1. -fastPow(x,1.f-0.0228f)/x/x/std::pow(x,-1.0228f)<< std::endl;
 
   return 0;
 
