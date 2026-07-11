18b4fcf40341a801ad684b681ed7ebc6bcdb74e5
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2026-07-11 11:24:09 +0200

    bench secosh

---
+++ floatPrec/benchCoth.cpp
@@ -0,0 +1,65 @@
+#include <benchmark/benchmark.h>
+#include<cmath>
+
+
+
+template<typename T>
+struct naive {
+   using Float=T;
+   T operator()(T x) { return std::sin(T(2)*std::atan(std::exp(x)));}
+};
+
+template<typename T>
+struct secosh {
+   using Float=T;
+   T operator()(T x) { return T(1)/std::cosh(x);}
+};
+
+
+template<typename T>
+struct poly {
+   using Float=T;
+   T operator()(T x) {
+     return 
+     T(0.999999986449) + x*(T(2.06961523705e-6) + x*( T(-0.500073072319) + x*( T(0.00106750423055) + x*( T(0.200707822535) + x*( T(0.0295750777896) + x*( T(-0.150307883454) + x*( T(0.0821370891714) + x*T(-0.0150543655971)
+      )))))));
+   }
+};
+
+template<typename F>
+void loop(benchmark::State& state) {
+   using T = F::Float;
+//   using F = secosh<float>;
+   F f;
+   T sum=0;
+   for (auto _ : state) {
+     benchmark::DoNotOptimize(sum);
+     for (T x=T(-1);  x<T(1); x+=T(1.e-6))
+       sum += f(x);
+     benchmark::DoNotOptimize(sum);
+   }
+}
+
+
+void nf(benchmark::State& state) { loop<naive<float>>(state);}
+void nd(benchmark::State& state) { loop<naive<double>>(state);}
+
+void sf(benchmark::State& state) { loop<secosh<float>>(state);}
+void sd(benchmark::State& state) { loop<secosh<double>>(state);}
+
+void pf(benchmark::State& state) { loop<poly<float>>(state);}
+void pd(benchmark::State& state) { loop<poly<double>>(state);}
+
+
+
+BENCHMARK(nf);
+BENCHMARK(nd);
+BENCHMARK(sf);
+BENCHMARK(sd);
+BENCHMARK(pf);
+BENCHMARK(pd);
+
+
+
+BENCHMARK_MAIN();
+
