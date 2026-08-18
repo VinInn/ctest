b057f4b68dbc84f2dc0ed2d47f6552ac98002563
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2026-08-18 11:14:54 +0200

    protect fma

--- floatPrec/Horner.h
+++ floatPrec/Horner.h
@@ -2,7 +2,11 @@
 #include<cmath>
 template<int N>
 float horner(float x, float const * const c) {
+#if  defined(__FMA__) || defined(FP_FAST_FMA)
   return std::fma(x,horner<N-1>(x,c),c[N]);
+#else
+  return x*horner<N-1>(x,c)+c[N];
+#endif
 }
 
 template<>
